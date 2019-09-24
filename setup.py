c3c19a66f64b7a910d57604f75283af91f1285fd
Steven Herbst <sherbst@stanford.edu>
2019-09-23 17:42:00 -0700

    very preliminary testing infrastucture

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='dragonphy',
+    version='0.0.1',
+    description='Open Source PHY v2',
+    scripts=[],
+    packages=[
+        'dragonphy',
+    ],
+    install_requires=[
+    ],
+    license='Apache License 2.0',
+    url='https://github.com/StanfordVLSI/dragonphy',
+    author='Stanford University',
+    python_requires='>=3.7',
+)
