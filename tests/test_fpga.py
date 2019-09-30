21e2fdbace5213f0191664ae8a632e7632ca141d
Steven Herbst <sherbst@stanford.edu>
2019-09-29 20:14:06 -0700

    fix broken tests

--- tests/test_fpga.py
+++ tests/test_fpga.py
@@ -2,8 +2,9 @@ from dragonphy import *
 
 def test_fpga():
     src = get_file('stim/fpga_stim.sv')
+    inc = get_file('inc/signals/fpga_verif/signals.sv')
     libs = get_deps(src, view_order=['fpga_verif', 'fpga'])
-    run_sim(srcs=[src], libs=libs, cwd=get_dir('tests/build_fpga'),
+    run_sim(srcs=[src, inc], libs=libs, cwd=get_dir('tests/build_fpga'),
             top='stim', inc_dirs=get_dirs('inc/signals/fpga_verif'))
 
 if __name__ == '__main__':
--- tests/test_loopback.py
+++ tests/test_loopback.py
@@ -2,8 +2,9 @@ from dragonphy import *
 
 def test_loopback():
     src = get_file('stim/loopback_stim.sv')
+    inc = get_file('inc/signals/beh/signals.sv')
     libs = get_deps(src, view_order=['beh'])
-    run_sim(srcs=[src], libs=libs, cwd=get_dir('tests/build_loopback'),
+    run_sim(srcs=[src, inc], libs=libs, cwd=get_dir('tests/build_loopback'),
             top='stim', inc_dirs=get_dirs('inc/signals/beh'))
 
 if __name__ == '__main__':
