eb2ca7bfcf8ec331e41c07944d6945fde4ba870b
Steven Herbst <sherbst@stanford.edu>
2019-09-29 20:14:06 -0700

    fix broken tests

--- tests/test_probes.py
+++ tests/test_probes.py
@@ -2,8 +2,9 @@ from dragonphy import *
 
 def test_probes():
     src = get_file('stim/loopback_stim.sv')
+    inc = get_file('inc/signals/beh/signals.sv')
     libs = get_deps(src, view_order=['beh'])
-    run_sim(srcs=[src], libs=libs, cwd=get_dir('tests/build_loopback'),
+    run_sim(srcs=[src, inc], libs=libs, cwd=get_dir('tests/build_probes'),
             top='stim', inc_dirs=get_dirs('inc/signals/beh'),
             probes=['stim'])
 
