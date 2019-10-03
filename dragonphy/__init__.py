d3156537a8184267e0addef9b52da2fab8eb57cc
Zachary Myers <zmagnitar@gmail.com>
2019-10-03 01:09:59 -0700

    Added a tester module, extended packager and modified style of system.yml

--- dragonphy/__init__.py
+++ dragonphy/__init__.py
@@ -2,6 +2,7 @@
 from .cmd 			import CmdLineParser
 from .packager      import Packager
 from .config 		import Configuration
+from .tester		import Tester
 # Some Python Verification Libraries
 from .channel   	import Channel, Filter
 from .adaptation 	import Wiener
