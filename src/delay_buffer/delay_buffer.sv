451ce2ae9357761a07b6c3ddfc5c2a46648b7fdc
Zachary Myers <zmagnitar@gmail.com>
2019-10-11 01:03:51 -0700

    Added untested buffer models

---
+++ src/delay_buffer/delay_buffer.sv
@@ -0,0 +1,39 @@
+module delay_buffer #(
+	parameter integer numChannels = 16,
+	parameter integer bitwidth 	  = 8,
+	parameter integer depth       = 5
+) (
+	input wire logic [bitwidth-1:0] in [numChannels-1:0],
+
+	input wire logic clk,
+	input wire logic rstb,
+
+	output reg [bitwidth-1:0] out [numChannels-1:0]
+);
+
+parameter integer intern_depth = depth-1;
+
+logic [bitwidth-1:0] internal_pipeline [numChannels-1:0][intern_depth-1:0];
+integer ii;
+
+genvar gi;
+generate
+	for(gi=0; gi<numChannels;gi=gi+1) begin
+		always_ff @(posedge clk or negedge rstb) begin
+			if(~rstb) begin
+				 out[gi] <= 0;
+				 for(ii=0;ii<intern_depth; ii=ii+1) begin
+				 	internal_pipeline[gi][ii] <= 0;
+				 end
+			end else begin
+				internal_pipeline[gi][0] <= in[gi];
+				for(ii=1; ii<intern_depth; ii=ii+1) begin
+					internal_pipeline[gi][ii] <= internal_pipeline[gi][ii-1];
+				end
+				out[gi] <= internal_pipeline[gi][intern_depth-1];
+			end
+		end
+	end
+endgenerate
+
+endmodule : delay_buffer
