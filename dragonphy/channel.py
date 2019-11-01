5df6c810a778739cf5102e8b2d921d92fa624beb
Olivia Hsu <owhsu@stanford.edu>
2019-10-31 20:54:01 -0700

    fix channel model to dielectric effect

--- dragonphy/channel.py
+++ dragonphy/channel.py
@@ -2,156 +2,176 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 class Filter:
-	def __init__(self, sampl_rate=1):
-		self.sampl_rate = sampl_rate
+    def __init__(self, sampl_rate=1):
+        self.sampl_rate = sampl_rate
 
-	def __getitem__(self, ind):
-		return self.impulse_response[ind]
+    def __getitem__(self, ind):
+        return self.impulse_response[ind]
 
-	def __call__(self, symbols):
-		#Needs further analysis - do I need to drop leading and lagging samples?
-		return np.convolve(symbols, self.impulse_response, mode='full')
+    def __call__(self, symbols):
+        #Needs further analysis - do I need to drop leading and lagging samples?
+        return np.convolve(symbols, self.impulse_response, mode='full')
 
-	def length(self):
-		return len(self.impulse_response)
+    def length(self):
+        return len(self.impulse_response)
 
 class Channel(Filter):
-	def __init__(self, channel_type='perfect', resp_depth=10, cursor_pos=2, sampl_rate=1, **kwargs):
-		super().__init__(sampl_rate)
-		self.resp_depth       = resp_depth
-		self.cursor_pos   	  = cursor_pos
-		self.impulse_response = self.channel_generator[channel_type](self, resp_depth=self.resp_depth, **kwargs)
-		self.reset_cursor()
-		self.real_time 		  = (np.array(range(self.length()))-self.cursor_pos)*1.0/sampl_rate
+    def __init__(self, channel_type='perfect', resp_depth=10, cursor_pos=2, sampl_rate=1, **kwargs):
+        super().__init__(sampl_rate)
+        self.resp_depth       = resp_depth
+        self.cursor_pos       = cursor_pos
+        self.impulse_response = self.channel_generator[channel_type](self, resp_depth=self.resp_depth, **kwargs)
+        self.reset_cursor()
+        self.real_time        = (np.array(range(self.length()))-self.cursor_pos)*1.0/sampl_rate
 
-	def __call__(self, symbols):
-		return super().__call__(symbols)*1.0/self.sampl_rate
-		
-	def normalize_area(self, unnormalized_array):
-		scale = 1/np.sum(unnormalized_array)*self.sampl_rate
-		return scale*unnormalized_array
+    def __call__(self, symbols):
+        return super().__call__(symbols)*1.0/self.sampl_rate
+        
+    def normalize_area(self, unnormalized_array):
+        scale = 1/np.sum(unnormalized_array)*self.sampl_rate
+        return scale*unnormalized_array
 
-	def normalize_energy(self, unnormalized_array):
-		scale = 1/np.sqrt(np.sum(np.square(unnormalized_array))*1.0/self.sampl_rate)
-		return scale*unnormalized_array
+    def normalize_energy(self, unnormalized_array):
+        scale = 1/np.sqrt(np.sum(np.square(unnormalized_array))*1.0/self.sampl_rate)
+        return scale*unnormalized_array
 
-	def normalize_none(self, unnormalized_array):
-		return unnormalized_array
+    def normalize_none(self, unnormalized_array):
+        return unnormalized_array
 
-	def reset_cursor(self):
-		self.cursor_pos = list(self.impulse_response).index(max(self.impulse_response))
+    def reset_cursor(self):
+        self.cursor_pos = list(self.impulse_response).index(max(self.impulse_response))
 
-	def calculate_time(self):
-		return (np.array(range(self.length()))-self.cursor_pos)*1.0/self.sampl_rate
+    def calculate_time(self):
+        return (np.array(range(self.length()))-self.cursor_pos)*1.0/self.sampl_rate
 
-	def perfect_channel(self, resp_depth=10, **kwargs):
-		return np.identity(resp_depth)[self.cursor_pos]
+    def perfect_channel(self, resp_depth=10, **kwargs):
+        return np.identity(resp_depth)[self.cursor_pos]
 
-	# From: The Physics of Transmission Lines at High and Very High Frequencies
-	# Continuous Model: h(t) = 1/(pi*k) * 1/(1+(t/k)^2)
-	def dielectric2_channel(self, resp_depth=10, normal='area', **kwargs):
-		try:
-			tau = kwargs['tau']
-		except KeyError as e:
-			print("Dielectric Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
-			tau = 2
-		
-		sample_per = 1.0/self.sampl_rate
-		
-		func_scale = 1.0/(np.pi*tau)
-		nT_over_tau = np.divide(range(-self.cursor_pos, resp_depth-self.cursor_pos), np.repeat(sample_per/tau, resp_depth))
-		
-		unscaled_array = 1.0/(1.0 + nT_over_tau**2)
-		return self.normal_select[normal](self, unscaled_array)
+    # From: The Physics of Transmission Lines at High and Very High Frequencies
+    # Continuous Model: h(t) = 1/(pi*k) * 1/(1+(t/k)^2)
+    def dielectric2_channel(self, resp_depth=10, normal='area', **kwargs):
+        try:
+            tau = kwargs['tau']
+        except KeyError as e:
+            print("Dielectric Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
+            tau = 2
+        
+        sample_per = 1.0/self.sampl_rate
+        
+        self.cursor_pos = int(resp_depth/2)
 
-	# From: Ken Boesch Thesis
-	# Continuous Model: h(t) = 1/(pi*k) * (1+t/k)/(1+(t/k)^2)
-	def dielectric1_channel(self, resp_depth=10, normal='area', **kwargs):
-		try:
-			tau = kwargs['tau']
-		except KeyError as e:
-			print("Dielectric Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
-			tau = 2
-		
-		sample_per = 1.0/self.sampl_rate
-		
-		func_scale = 1.0/(np.pi*tau)
-		nT_over_tau = np.divide(range(-self.cursor_pos, resp_depth-self.cursor_pos), np.repeat(sample_per/tau, resp_depth))
-		
-		unscaled_array = np.clip((1.0 + nT_over_tau)/(1.0 + nT_over_tau**2), 0, None)
-		return self.normal_select[normal](self, unscaled_array)
+        func_scale = 1.0/(np.pi*tau)
+        nT_over_tau = np.divide(range(-self.cursor_pos, resp_depth-self.cursor_pos), np.repeat(sample_per/tau, resp_depth))
+        
+        unscaled_array = 1.0/(1.0 + nT_over_tau**2)
+        return self.normal_select[normal](self, unscaled_array)
 
-	# From: The Physics of Transmission Lines at High and Very High Frequencies
-	# Continuous Model:     h(t) = sqrt(tau)/(2*t*sqrt(pi*t)) * e^(-tau/4t)
-	# Discrete Model:       h(n) = sqrt(beta/pi) * e^(-beta/n)/n^(3/2)
-	def skineffect_channel(self, resp_depth=10, normal='area', **kwargs):
-		try: 
-			tau = kwargs['tau']
-		except KeyError as e:
-			print("Skin Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
-			tau = 2
+    # From: Ryan Boesch Thesis
+    # Continuous Model: h(t) = 1/(pi*k) * (1+t/k)/(1+(t/k)^2)
+    def dielectric1_channel(self, resp_depth=10, normal='area', **kwargs):
+        try:
+            tau = kwargs['tau']
+        except KeyError as e:
+            print("Dielectric Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
+            tau = 2
+        
+        sample_per = 1.0/self.sampl_rate
+       
+        func_scale = 1.0/(np.pi*tau)
+        nT_over_tau = np.divide(range(-int(1/tau) - 1 , resp_depth-int(1/tau) - 1), np.repeat(sample_per/tau, resp_depth))
+        
+        unscaled_array = np.pad(np.clip((1.0 + nT_over_tau)/(1.0 + nT_over_tau**2), 0, None), (self.cursor_pos, 0), 'constant', constant_values=(0,0))[0:resp_depth]
+        return self.normal_select[normal](self, unscaled_array)
 
-		sampl_per = 1.0/self.sampl_rate
+    # From: The Physics of Transmission Lines at High and Very High Frequencies
+    # Continuous Model:     h(t) = sqrt(tau)/(2*t*sqrt(pi*t)) * e^(-tau/4t)
+    # Discrete Model:       h(n) = sqrt(beta/pi) * e^(-beta/n)/n^(3/2)
+    def skineffect_channel(self, resp_depth=10, normal='area', **kwargs):
+        try: 
+            tau = kwargs['tau']
+        except KeyError as e:
+            print("Skin Effect Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
+            tau = 2
+
+        sampl_per = 1.0/self.sampl_rate
         # Let beta be tau/(4*T)
-		beta = tau/(4*sampl_per)
+        beta = tau/(4*sampl_per)
 
         # func_scale = np.sqrt(beta/np.pi)
-		expon_val = np.repeat(np.exp(-beta), resp_depth-1)
-		one_over_n = np.divide(np.ones((resp_depth-1,)), range(1,resp_depth))
-		expon_arr = np.power(expon_val, one_over_n)
-		n_to_3_o_2 = np.power(range(1,resp_depth), 3.0/2.0)
+        expon_val = np.repeat(np.exp(-beta), resp_depth-1)
+        one_over_n = np.divide(np.ones((resp_depth-1,)), range(1,resp_depth))
+        expon_arr = np.power(expon_val, one_over_n)
+        n_to_3_o_2 = np.power(range(1,resp_depth), 3.0/2.0)
 
-		# Shift is equivalent to convolving with a delayed impulse response. Delay by 1 more for n=0 (cannot divide by 0).
-		unscaled_array = np.pad(np.divide(expon_arr,n_to_3_o_2),(self.cursor_pos,0),'constant', constant_values=(0,0))[0:resp_depth]
-		return self.normal_select[normal](self, unscaled_array)
+        # Shift is equivalent to convolving with a delayed impulse response. Delay by 1 more for n=0 (cannot divide by 0).
+        unscaled_array = np.pad(np.divide(expon_arr,n_to_3_o_2),(self.cursor_pos,0),'constant', constant_values=(0,0))[0:resp_depth]
+        return self.normal_select[normal](self, unscaled_array)
 
-	def exponential_channel(self, resp_depth=10, normal='area', **kwargs):
-		try: 
-			tau = kwargs['tau']
-		except KeyError:
-			print("Exponential Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
-			tau = 2
-		expon_val = np.exp(-1.0/(tau*self.sampl_rate))
-		expon_val_sqrd = np.exp(-2.0/(tau*self.sampl_rate))
-		expon_normal_select = { 'area' : self.sampl_rate*(1-expon_val), 'energy' :  np.sqrt(self.sampl_rate*(1-expon_val_sqrd))}
+    # Combined skineffect and dielectric effect channel model
+    def combined_channel(self, resp_depth=10, normal='area', **kwargs):
+        try:
+            tau_skin = kwargs['tau1']
+            tau_diel = kwargs['tau2']
+        except KeyError as e:
+            print("Skin Effect Channel: {} not specified, defaulting to {}=2".format('tau1', 'tau1'))
+            print("Dielectric Channel: {} not specified, defaulting to {}=2".format('tau2', 'tau2'))
+            tau_skin = 2
+            tau_diel = 2
 
-		return expon_normal_select[normal]*np.pad(np.power(np.repeat(expon_val, resp_depth-self.cursor_pos), range(resp_depth-self.cursor_pos)),(self.cursor_pos,0),'constant', constant_values=(0,0))
+        diel_resp = self.dielectric1_channel(resp_depth/2, normal, tau_diel)
+        skin_resp = self.skineffect_channel(resp_depth/2, normal, tau_skin)
 
-	def modify_channel(self, channel_type, normal='area', resp_depth_ext=10, **kwargs):
-		modifier_response = self.channel_generator[channel_type](self, resp_depth=resp_depth_ext, normal=normal, **kwargs)
-		self.resp_depth=self.resp_depth+resp_depth_ext
-		self.impulse_response = self.normal_select[normal](self, np.convolve(self.impulse_response, modifier_response, mode='full')*1.0/self.sampl_rate)
-		self.reset_cursor()
-		self.real_time        = self.calculate_time()
-
-	def calculate_channel_area(self):
-		return np.sum(self.impulse_response)*1.0/self.sampl_rate
-
-	def calculate_channel_energy(self):
-		return np.sum(np.square(self.impulse_response))*1.0/self.sampl_rate
-
-	def calculate_settled_step_response(self):
-		return np.sum(self.impulse_response)*1.0/self.sampl_rate
+        unscaled_array = np.convolve(diel_resp, skin_resp)
+        return self.normal_select[normal](self, unscaled_array)
 
 
-	channel_generator = { 'perfect' : perfect_channel, 'exponent' : exponential_channel, 'skineffect' : skineffect_channel, 'dielectric2' : dielectric2_channel, \
-						 'dielectric1' : dielectric1_channel}
-	normal_select     = { 'area' : normalize_area, 'energy' : normalize_energy, 'none': normalize_none}
+    def exponential_channel(self, resp_depth=10, normal='area', **kwargs):
+        try: 
+            tau = kwargs['tau']
+        except KeyError:
+            print("Exponential Channel: {} not specified, defaulting to {}=2".format('tau','tau'))
+            tau = 2
+        expon_val = np.exp(-1.0/(tau*self.sampl_rate))
+        expon_val_sqrd = np.exp(-2.0/(tau*self.sampl_rate))
+        expon_normal_select = { 'area' : self.sampl_rate*(1-expon_val), 'energy' :  np.sqrt(self.sampl_rate*(1-expon_val_sqrd))}
+
+        return expon_normal_select[normal]*np.pad(np.power(np.repeat(expon_val, resp_depth-self.cursor_pos), range(resp_depth-self.cursor_pos)),(self.cursor_pos,0),'constant', constant_values=(0,0))
+
+    def modify_channel(self, channel_type, normal='area', resp_depth_ext=10, **kwargs):
+        modifier_response = self.channel_generator[channel_type](self, resp_depth=resp_depth_ext, normal=normal, **kwargs)
+        self.resp_depth=self.resp_depth+resp_depth_ext
+        self.impulse_response = self.normal_select[normal](self, np.convolve(self.impulse_response, modifier_response, mode='full')*1.0/self.sampl_rate)
+        self.reset_cursor()
+        self.real_time        = self.calculate_time()
+
+    def calculate_channel_area(self):
+        return np.sum(self.impulse_response)*1.0/self.sampl_rate
+
+    def calculate_channel_energy(self):
+        return np.sum(np.square(self.impulse_response))*1.0/self.sampl_rate
+
+    def calculate_settled_step_response(self):
+        return np.sum(self.impulse_response)*1.0/self.sampl_rate
+
+
+    channel_generator = { 'perfect' : perfect_channel, 'exponent' : exponential_channel, 'skineffect' : skineffect_channel, 'dielectric2' : dielectric2_channel, \
+                         'dielectric1' : dielectric1_channel, 'combined' : combined_channel}
+    normal_select     = { 'area' : normalize_area, 'energy' : normalize_energy, 'none': normalize_none}
 
 # Used for testing 
 if __name__ == "__main__":
-	c1 = Channel(channel_type='skineffect', sampl_rate=5, resp_depth=10)
-	c2 = Channel(channel_type='dielectric1', sampl_rate=5, resp_depth=125)
-	c3 = Channel(channel_type='dielectric2', sampl_rate=5, resp_depth=125)
+    c1 = Channel(channel_type='skineffect', sampl_rate=5, resp_depth=10)
+    c2 = Channel(channel_type='dielectric1', sampl_rate=5, resp_depth=125)
+    c3 = Channel(channel_type='dielectric2', sampl_rate=5, resp_depth=125)
 
-	print(c1.impulse_response)
-	plt.plot(c1.impulse_response)
-	plt.show()
+    print(c1.impulse_response)
+    plt.plot(c1.impulse_response)
+    plt.show()
 
-	print(c2.impulse_response)	
-	plt.plot(c2.impulse_response)
-	plt.show()
+    print(c2.impulse_response)  
+    plt.plot(c2.impulse_response)
+    plt.show()
 
-	print(c3.impulse_response)	
-	plt.plot(c3.impulse_response)
-	plt.show()
+    print(c3.impulse_response)  
+    plt.plot(c3.impulse_response)
+    plt.show()
--- verif/analysis/system.py
+++ verif/analysis/system.py
@@ -7,72 +7,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_parametric_channel():
-    tau = [2, 8, 11, 16, 32, 64]
-    resp_len_skin = [int(np.exp(np.sqrt(ta/64.0)-1)*300) for ta in tau]  #[5, 10, 50, 100, 150]
-    resp_len_diel = [int(((1+np.abs(ta/64.0))/2)*20) for ta in tau]  #[5, 10, 50, 100, 150]
+    tau1 = [2, 8, 11, 16, 32, 64]
+    tau2 = [2, 1, 0.5, 0.25, 0.1, 0.05]
+    resp_len_skin = [int(np.exp(np.sqrt(ta/64.0)-1)*300) for ta in tau1]  #[5, 10, 50, 100, 150]
+    resp_len_diel = [int(((1+np.abs(1/ta/20))/2)*20) for ta in tau2]  #[5, 10, 50, 100, 150]
     pos = 2
 
     f, ax = plt.subplots(2, 2) 
 
-    ax[0, 0].set_title('Skineffect Channel, tau')
-    ax[1, 0].set_title('Dialectric Channel, tau')
+    ax[0, 0].set_title('Skineffect Channel, len=50')
+    ax[1, 0].set_title('Dielectric Channel, len=50')
     # Create all channels
-    for t in tau: 
+    for t in tau1: 
         chan_skin = Channel(channel_type='skineffect', normal='area', tau=t, sampl_rate=1, cursor_pos=pos, resp_depth=50)
-        chan_dial = Channel(channel_type='dielectric1', normal='area', tau=t, sampl_rate=1, cursor_pos=pos, resp_depth=50)
         ax[0, 0].plot(chan_skin.impulse_response, label='tau='+str(t))
-        ax[1, 0].plot(chan_dial.impulse_response, label='tau='+str(t))
 
-    ax[0, 1].set_title('Skineffect Channel, len')
-    ax[1, 1].set_title('Dialectric Channel, len')
+    for t in tau2: 
+        chan_dia1 = Channel(channel_type='dielectric1', normal='area', tau=t, sampl_rate=1, cursor_pos=pos, resp_depth=50)
+    
+        ax[1, 0].plot(chan_dia1.impulse_response, label='tau='+str(t))
+
+    ax[0, 0].legend()
+    ax[1, 0].legend()
+
+    ax[0, 1].set_title('Skineffect Channel, tau=16')
+    ax[1, 1].set_title('Dielectric Channel, tau=16')
     # Create all channels
     for r_s, r_d in zip(resp_len_skin, resp_len_diel): 
         chan_skin = Channel(channel_type='skineffect', normal='area', tau=11, sampl_rate=1, cursor_pos=pos, resp_depth=r_s)
-        chan_dial = Channel(channel_type='dielectric1', normal='area', tau=11, sampl_rate=1, cursor_pos=pos, resp_depth=r_d)
+        chan_dial = Channel(channel_type='dielectric1', normal='area', tau=1, sampl_rate=1, cursor_pos=pos, resp_depth=r_d)
         ax[0, 1].plot(chan_skin.impulse_response, label='len='+str(r_s))
         ax[1, 1].plot(chan_dial.impulse_response, label='len='+str(r_d))
     
-    f.legend()
+    ax[0, 1].legend()
+    ax[1, 1].legend()
     plt.show()
 
 # Run the full system in python (w/ cheating).
 # Uses ideal Wiener filter, uniform quantizer, and ideal channel model for
 # MLSD
 def test_system_cheating():
+    # Skineffect Channel Parameters:
+    #   tau = 9.5  (~30db down)
+    #   len = 150
+
+
     # Initialize parameters 
-    iterations = 500000
+    load_codes = False
+    iterations = 100000
     pos = 1
-    resp_len = 130
-    bitwidth = 5
+    resp_len = 150
+    bitwidth = 6
     plot_len = 100 #iterations
     num_taps = 3
     noise_amp = 0.02
-    noise_en = False
-    tau = 11
+    noise_en = True
+    tau = 0.87
     
-    # Generate Ideal Codes
-    ideal_codes = np.random.randint(2, size=iterations)*2 - 1
-    np.savetxt('verif/analysis/ideal_codes.txt', ideal_codes)
    
-    # Load Ideal Codes
-    ideal_codes = np.loadtxt('verif/analysis/ideal_codes.txt', dtype=np.int16)
-    print(f'Ideal codes: {ideal_codes[:plot_len]}')
+    if load_codes:
+        # Load Ideal Codes
+        ideal_codes = np.loadtxt('verif/analysis/ideal_codes.txt', dtype=np.int16)
+        print(f'Ideal codes: {ideal_codes[:plot_len]}')
+        iterations=len(ideal_codes)
+    else:
+        # Generate Ideal Codes
+        #ideal_codes = np.tile(np.array([-1, 1]), int(iterations/2))    # Harmonic Codes
+        ideal_codes = np.random.randint(2, size=iterations)*2 - 1       # Random Codes
+        np.savetxt('verif/analysis/ideal_codes.txt', ideal_codes)
+
 
     # Create channel model and channel output
-    chan = Channel(channel_type='skineffect', normal='area', tau=tau, sampl_rate=1, cursor_pos=pos, resp_depth=resp_len)
+    chan = Channel(channel_type='dielectric1', normal='area', tau=tau, sampl_rate=1, cursor_pos=pos, resp_depth=resp_len)
     chan_out = chan(ideal_codes)
+
+    # Plot channel model
+    plt.plot(chan.impulse_response)
+    plt.suptitle("Channel Response")
+    plt.show()
     print(f'Channel resp: {chan.impulse_response}')
     print(f'Channel out: {chan_out[:plot_len]}')
     print(f'Cursor pos: {chan.cursor_pos}')
+    pos = chan.cursor_pos
 
     # Add noise
     if noise_en:
-        chan_out = chan_out + np.random.randn(len(chan_out))*0.02
+        chan_out = chan_out + np.random.randn(len(chan_out))*noise_amp
    
     # Quantize channel output
     qc = Quantizer(bitwidth, signed=True)
     quantized_chan_out = qc.quantize_2s_comp(chan_out)
-
 
     # Adapt Wiener filter to quantized channel output
     adapt = Wiener(step_size = 0.1, num_taps = num_taps, cursor_pos=pos)
@@ -93,6 +117,7 @@ def test_system_cheating():
     plot_comparison(quantized_chan_out, ideal_codes, delay_ffe=pos, scale=50, labels=["quantized chan", "ideal"])
     plot_comparison(ffe_out, ideal_codes, delay_ffe=pos, scale=15000, labels=["ffe out", "ideal"])
     plot_multi_hist(ffe_out, ideal_codes, delay_ffe=pos, n_bits=4, bit_pos=1) 
+
 
     # Make decision based on FFE output
     comp_out = np.array([1 if int(np.floor(ffe_val)) >= 0 else -1 for ffe_val in ffe_out])
@@ -126,7 +151,7 @@ def test_system_cheating():
     print(f'FFE Decision Output: {corrected_comp_out[:plot_len]}')
 
     ffe_errors = np.inner(corrected_comp_out - ideal_codes, corrected_comp_out - ideal_codes)/4
-    print(f'Number of FFE mismatches: {ffe_errors}')
+    print(f'Number of FFE mismatches: {ffe_errors} \t BER: {ffe_errors/iterations}')
     err_idx = [i for i in range(len(corrected_comp_out)) if corrected_comp_out[i] != ideal_codes[i]]
     print(f'Mismatches occur: {err_idx}')
 
@@ -142,7 +167,7 @@ def test_system_cheating():
 
     err_idx = [i for i in range(len(mlsd_out_ffe_err)) if mlsd_out_ffe_err[i] != ideal_codes[i]]
     print(f'Mismatches occur: {err_idx}')
-    print(f'Num mismatch: {len(err_idx)}')
+    print(f'Num mismatch: {len(err_idx)} \t BER: {len(err_idx)/iterations}')
     
 
     #plt.figure()
@@ -160,7 +185,7 @@ def test_system_cheating():
 
     err_idx = [i for i in range(len(mlsd_out_no)) if mlsd_out_no[i] != ideal_codes[i]]
     print(f'Mismatches occur: {err_idx}')
-    print(f'Num mismatch: {len(err_idx)}')
+    print(f'Num mismatch: {len(err_idx)} \t BER: {len(err_idx)/iterations}')
 
 
     # MLSD with update
@@ -169,7 +194,7 @@ def test_system_cheating():
 
     err_idx = [i for i in range(len(mlsd_out)) if mlsd_out[i] != ideal_codes[i]]
     print(f'Mismatches occur: {err_idx}')
-    print(f'Num mismatch: {len(err_idx)}')
+    print(f'Num mismatch: {len(err_idx)} \t BER: {len(err_idx)/iterations}')
 
 
     if len(err_idx) == 0:
@@ -178,8 +203,8 @@ def test_system_cheating():
         print(f'TEST FAILED') 
 
 def main():
-    test_system_cheating()
     #plot_parametric_channel()
+    test_system_cheating()
 
 # Run the digital backend for the RX system in Python
 if __name__ == "__main__":
