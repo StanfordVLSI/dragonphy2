7c91e7da505d28f06894b7a81b35842188b582af
Olivia Hsu <owhsu@stanford.edu>
2019-11-06 13:37:42 -0800

    Fix mlsd to do multi-bit analysis

--- verif/mlsd/mlsd.py
+++ verif/mlsd/mlsd.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 import scipy.special
 
 class MLSD:
-    def __init__(self, cursor_pos, chan_resp, n_pre=None, n_post=None, n_bits_after=0, n_bits_before=0, bitwidth=8, quantizer_lsb=1):
+    def __init__(self, cursor_pos, chan_resp, n_post=None, n_pre=None, n_future=0, n_prev=0, bitwidth=8, quantizer_lsb=1):
         self.chan_resp_depth = len(chan_resp)
         self.chan_resp = chan_resp
         self.chan_cursor_pos = cursor_pos
-        self.n_pre = self.chan_resp_depth - self.chan_cursor_pos - 1 if n_pre is None else n_pre
-        self.n_post = self.chan_cursor_pos if n_post is None else n_post
-        self.n_bits_after = n_bits_after
-        self.n_bits_before = n_bits_before
+        self.n_post = self.chan_resp_depth - self.chan_cursor_pos - 1 if n_post is None else n_post
+        self.n_pre = self.chan_cursor_pos if n_pre is None else n_pre
+        self.n_future = n_future
+        self.n_prev = n_prev
         self.bw = bitwidth    
         self.lsb = quantizer_lsb
 
@@ -20,20 +20,20 @@ class MLSD:
         qc = Quantizer(self.bw, lsb=self.lsb, signed=True)        
 
         difference = []
-        for i in range(-self.n_bits_before, self.n_bits_after + 1):
+        for i in range(-self.n_prev, self.n_future + 1):
             # Treat this as our "estimates" of the symbols
-            recovered_bits_windowed = recovered_bits[index + i - self.n_pre: index + i + self.n_post + 1]
+            recovered_bits_windowed = recovered_bits[index + i - self.n_post: index + i + self.n_pre + 1]
             recovered = np.copy(recovered_bits_windowed)
-            recovered[self.n_pre] = bit
+            recovered[self.n_post] = bit
 
             if debug:
-                print(f'{index + i - self.n_pre}, {index + i + self.n_post}')
+                print(f'{index + i - self.n_post}, {index + i + self.n_pre}')
                 print(f'Recovered win: {recovered_bits_windowed}')
                 print(f'Recovered 1: {recovered}')   
  
-            chan_start = self.chan_resp_depth - self.chan_cursor_pos - 1 - self.n_pre
-            chan_end = self.chan_resp_depth + (self.n_post - self.chan_cursor_pos)
-            chan_resp_modified = self.chan_resp[::-1][chan_start:chan_end] 
+            chan_start = self.chan_resp_depth - self.chan_cursor_pos - 1 - self.n_post
+            chan_end = self.chan_resp_depth + (self.n_pre - self.chan_cursor_pos)
+            chan_resp_modified = self.chan_resp[chan_start:chan_end] 
 
             se = np.dot(chan_resp_modified, recovered)
             if debug:
@@ -42,39 +42,39 @@ class MLSD:
             q_se = qc.quantize_2s_comp(se)
             if debug:
                 print(q_se)
-                print(chan_out[index + i - self.n_pre + self.chan_cursor_pos]) 
+                print(chan_out[index + i - self.n_post + self.chan_cursor_pos]) 
             
-            chan_out_idx = index + i - self.n_pre + self.chan_cursor_pos
+            chan_out_idx = index + i - self.n_post + self.chan_cursor_pos
             difference.append(chan_out[chan_out_idx] - q_se)
 
         difference = np.inner(difference, difference)
         return difference
 
     def perform_mlsd_single(self, recovered_bits, chan_out, index=0, debug=False):
-        #assert(index >= (self.n_pre + self.n_bits_after))
-        #assert(len(recovered_bits) > index + self.n_post + self.n_bits_after)  
+        #assert(index >= (self.n_post + self.n_future))
+        #assert(len(recovered_bits) > index + self.n_pre + self.n_future)  
 
         qc = Quantizer(self.bw, lsb=self.lsb, signed=True)        
 
-        difference_1 = []
-        difference_0 = []
-        for i in range(-self.n_bits_before, self.n_bits_after + 1):
+        difference = []
+        seq = []
+        for i in range(-self.n_prev, self.n_future + 1):
             # Treat this as our "estimates" of the symbols
-            recovered_bits_windowed = recovered_bits[index + i - self.n_pre: index + i + self.n_post + 1]
+            recovered_bits_windowed = recovered_bits[index + i - self.n_post: index + i + self.n_pre + 1]
             recovered_1 = np.copy(recovered_bits_windowed)
-            recovered_1[self.n_pre] = 1
+            recovered_1[self.n_post] = 1
 
             recovered_0 = np.copy(recovered_bits_windowed)
-            recovered_0[self.n_pre] = -1
+            recovered_0[self.n_post] = -1
             
             if debug:
-                print(f'{index + i - self.n_pre}, {index + i + self.n_post}')
+                print(f'{index + i - self.n_post}, {index + i + self.n_pre}')
                 print(f'Recovered win: {recovered_bits_windowed}')
                 print(f'Recovered 1: {recovered_1}')   
                 print(f'Recovered 0: {recovered_0}')   
  
-            chan_start = self.chan_resp_depth - self.chan_cursor_pos - 1 - self.n_pre
-            chan_end = self.chan_resp_depth + (self.n_post - self.chan_cursor_pos)
+            chan_start = self.chan_resp_depth - self.chan_cursor_pos - 1 - self.n_post
+            chan_end = self.chan_resp_depth + (self.n_pre - self.chan_cursor_pos)
             chan_resp_modified = self.chan_resp[::-1][chan_start:chan_end] 
 
             se1 = np.dot(chan_resp_modified, recovered_1)
@@ -88,21 +88,32 @@ class MLSD:
             if debug:
                 print(q_se1)
                 print(q_se0)
-                print(chan_out[index + i - self.n_pre + self.chan_cursor_pos]) 
+                print(chan_out[index + i - self.n_post + self.chan_cursor_pos]) 
             
-            chan_out_idx = index + i - self.n_pre + self.chan_cursor_pos
-            difference_1.append(chan_out[chan_out_idx] - q_se1)
-            difference_0.append(chan_out[chan_out_idx] - q_se0) 
+            chan_out_idx = index + i - self.n_post + self.chan_cursor_pos
+            difference = self.combinations(chan_out[chan_out_idx] - q_se0, chan_out[chan_out_idx] - q_se1, difference)
+            seq = self.combinations(-1, 1, seq)
 
-        difference_1 = np.inner(difference_1, difference_1)
-        difference_0 = np.inner(difference_0, difference_0)
+        difference = [np.inner(x, x) for x in np.array(difference)]
     
         if debug:
-            print(f'Difference assuming 1: {difference_1}')
-            print(f'Difference assuming 0: {difference_0}')
+            print(f'Difference: {difference}')
 
-        bit_decision = -1 if difference_1 > difference_0 else 1
-        return bit_decision
+        bit_decision = np.argmin(difference) 
+        return seq[bit_decision]
+
+    def combinations(self, n1, n2, arr):
+        result = []
+        if arr == []:
+            result.append([n1])
+            result.append([n2])
+        else: 
+            for a in arr:
+                sub1 = a + [n1]
+                sub2 = a + [n2]
+                result.append(sub1)
+                result.append(sub2)
+        return result
         
     def calc_mlsd_err(self, recovered_bits, chan_out, inv=False, debug=False):
         result = np.zeros(len(recovered_bits))
@@ -113,7 +124,7 @@ class MLSD:
 
         start_index = self.chan_resp_depth - self.chan_cursor_pos - 1
         end_index = len(r_bits_zero) - self.chan_cursor_pos
-        for i in range(start_index, end_index):
+        for i in range(start_index + self.n_prev, end_index - self.n_future):
             bit = r_bits_zero[i] if not inv else -1 * r_bits_zero[i]
             mlsd_out = self.calculate_norm(r_bits_zero, chan_out, i, bit, debug)
             result[i - start_index] = mlsd_out
@@ -135,20 +146,9 @@ class MLSD:
         f.legend()
         plt.show()
 
-    # This function is broken, do not use
-    def perform_mlsd_update(self, recovered_bits, chan_out, zero_pad=False):
-        start_index = self.chan_resp_depth - self.chan_cursor_pos - 1
-        end_index = len(recovered_bits) - self.chan_cursor_pos
+        return np.abs(ffe_out)
 
-        result = np.zeros(end_index - start_index)
-        for i in range(start_index, end_index):
-            mlsd_out = self.perform_mlsd_single(recovered_bits, chan_out, i)
-            recovered_bits[i] = mlsd_out 
-            result[i - start_index] = mlsd_out
-
-        return result
-
-    def perform_mlsd_zeros(self, recovered_bits, chan_out, bool_arr = None, update=True, debug=False):
+    def perform_mlsd_zeros(self, recovered_bits, chan_out, step_size=1, bool_arr = None, update=True, debug=False):
         result = np.zeros(len(recovered_bits))
 
         if bool_arr is None:
@@ -156,12 +156,11 @@ class MLSD:
 
         n_front_zeros = self.chan_resp_depth - self.chan_cursor_pos - 1
         n_back_zeros = self.chan_cursor_pos
-        print(recovered_bits)
         r_bits_zero = np.concatenate((np.zeros(n_front_zeros), recovered_bits, np.zeros(n_back_zeros)))
 
         start_index = self.chan_resp_depth - self.chan_cursor_pos - 1
         end_index = len(r_bits_zero) - self.chan_cursor_pos
-        for i in range(start_index, end_index):
+        for i in range(start_index + self.n_prev, end_index - self.n_future, step_size):
             if debug:
                 print(f'=========== Index: {i - start_index}')
 
@@ -173,7 +172,7 @@ class MLSD:
             if update:
                 r_bits_zero[i] = mlsd_out 
 
-            result[i - start_index] = mlsd_out
+            result[i - start_index - self.n_prev: i - start_index + self.n_future+ 1] = mlsd_out
 
         return result
 
@@ -181,7 +180,12 @@ class MLSD:
         ffe_norm = self.calc_mlsd_err(recovered_bits, chan_out) > threshold
         result = self.perform_mlsd_zeros(recovered_bits, chan_out, ffe_norm, update, debug)
         return result
-         
+        
+    def find_err_idx(self, mlsd, ideal, shift_pos=True, plot=True):
+        err_idx = [i for i in range(len(mlsd)) if mlsd[i] != ideal[i]]
+        print(f'Mismatches occur: {err_idx}')
+        print(f'Num mismatch: {len(err_idx)} \t BER: {len(err_idx)/len(mlsd)}')
+        return err_idx 
 
 def main():
     ideal = np.array([-1, -1, 1, -1, -1, 1, 1, 1, -1, -1, -1, -1])
