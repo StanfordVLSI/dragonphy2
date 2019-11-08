d1b3a873b2ded2cbe9ad8a1d49526e3829896b67
Steven Herbst <sherbst@stanford.edu>
2019-11-08 14:33:26 -0800

    using svreal v3

--- verif/chan/fpga/chan.sv
+++ verif/chan/fpga/chan.sv
@@ -5,6 +5,13 @@ module chan (
     `ANALOG_OUTPUT data_ana_o
 );
 
-    `SVREAL_ASSIGN(data_ana_i, data_ana_o);
+    generate
+        // alias I/O
+        `SVREAL_ALIAS_INPUT(data_ana_i.value, data_ana_i_value);
+        `SVREAL_ALIAS_OUTPUT(data_ana_o.value, data_ana_o_value);
+
+        // pass input straight to output
+        `SVREAL_ASSIGN(data_ana_i_value, data_ana_o_value);
+    endgenerate
 
 endmodule
