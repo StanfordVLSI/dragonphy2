d4ab66a42f5c0a8c2750a65cc5a15c1fd993682b
Steven Herbst <sherbst@stanford.edu>
2019-11-08 15:01:37 -0800

    upgrade to svreal v3, start moving dt implementation into svreal

--- verif/clk_gen/fpga/clk_gen.sv
+++ verif/clk_gen/fpga/clk_gen.sv
@@ -1,8 +1,8 @@
 `include "signals.sv"
 
 module clk_gen #(
-    parameter integer t_hi=1,
-    parameter integer t_lo=1
+    parameter real t_hi=0.5e-9,
+    parameter real t_lo=0.5e-9
 ) (
     output wire logic clk_o
 );
@@ -18,19 +18,19 @@ module clk_gen #(
     // because the dt_req and clk_val signals are
     // read hierarchically, even though they are
     // not outputs of the module
-    (* dont_touch = "true" *) `DECL_DT(dt_req);
+    (* dont_touch = "true" *) `DT_T dt_req;
     (* dont_touch = "true" *) logic clk_val;
     always @(posedge `EMU.clk) begin
         if (`EMU.rst == 1'b1) begin
             clk_val <= 1'b0;
-            dt_req <= t_lo;
+            dt_req <= `DT_CONST(t_lo);
         end else if (dt_req == `EMU.dt) begin
             if (clk_val == 1'b0) begin
                 clk_val <= 1'b1;
-                dt_req <= t_hi;
+                dt_req <= `DT_CONST(t_hi);
             end else begin
                 clk_val <= 1'b0;
-                dt_req <= t_lo;
+                dt_req <= `DT_CONST(t_lo);
             end
         end else begin
             clk_val <= clk_val;
