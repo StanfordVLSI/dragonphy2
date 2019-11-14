94ac82e68e102e42653c2d836af295ad1a3b072e
Olivia Hsu <owhsu@stanford.edu>
2019-11-14 14:19:01 -0800

    Move verif/analysis to dragonphy/analysis

--- dragonphy/__init__.py
+++ dragonphy/__init__.py
@@ -8,6 +8,7 @@ from .channel   	import Channel, Filter
 from .adaptation 	import Wiener
 from .quantizer 	import Quantizer
 from .ffe           import FFEHelper
+from .analysis.histogram import Histogram
 # Some ENOB, DNL extraction libraries - these should be merged into one!
 #from .adc 			import AdcChecker, AdcModel
 #from .ac  			import AdcAcChecker
