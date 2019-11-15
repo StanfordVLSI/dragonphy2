330dfc1cb3e5cc2c47b2035e6492fb3ddca2ec2b
Olivia Hsu <owhsu@stanford.edu>
2019-11-14 18:48:25 -0800

    Minor test script fixes and add syn to view_order for get_deps

--- tests/test_probes.py
+++ tests/test_probes.py
@@ -1,10 +1,17 @@
 from dragonphy import *
+from common import adapt_fir
 
 def test_probes():
+    build_dir  = 'tests/build_probes'
+
+    # Adapts the fir weights and returns the package files needed
+    config = 'test_loopback_config'
+    packages = adapt_fir(build_dir, config)
+
     src = get_file('stim/loopback_stim.sv')
     inc = get_file('inc/signals/beh/signals.sv')
-    libs = get_deps(src, view_order=['beh'])
-    run_sim(srcs=[src, inc], libs=libs, cwd=get_dir('tests/build_probes'),
+    libs = get_deps(src, view_order=['beh', 'syn'])
+    run_sim(srcs=[src, inc] + packages, libs=libs, cwd=get_dir(build_dir),
             top='stim', inc_dirs=get_dirs('inc/signals/beh'),
             probes=['stim'])
 
