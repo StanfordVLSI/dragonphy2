fbd9753a7e8eb09c083799a7053a75269f9df79e
Steven Herbst <sherbst@stanford.edu>
2020-01-16 16:42:35 -0800

    try updating to pypi versions of svreal / anasymod / msdsl

--- setup.py
+++ setup.py
@@ -1,19 +1,46 @@
 from setuptools import setup
 
+name = 'dragonphy'
+version = '0.1.0'
+
+DESCRIPTION = '''\
+Open Source PHY v2\
+'''
+
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
 setup(
-    name='dragonphy',
-    version='0.0.1',
-    description='Open Source PHY v2',
-    scripts=[],
+    name=name,
+    version=version,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords = ['high-speed', 'high speed', 'link', 'high-speed link',
+                'high speed link', 'analog', 'mixed-signal', 'mixed signal',
+                'generator', 'ic', 'integrated circuit', 'chip'],
     packages=[
-        'dragonphy'
+        f'{name}'
     ],
     install_requires=[
+        'anasymod',
         'pexpect', 
-        'pyyaml', 'numpy', 'matplotlib', 
+        'pyyaml',
+        'numpy',
+        'matplotlib'
     ],
     license='Apache License 2.0',
-    url='https://github.com/StanfordVLSI/dragonphy',
+    url=f'https://github.com/StanfordVLSI/{name}',
     author='Stanford University',
     python_requires='>=3.7',
+    download_url = f'https://github.com/StanfordVLSI/{name}/archive/v{version}.tar.gz',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'License :: OSI Approved :: Apache Software License',
+        f'Programming Language :: Python :: 3.7'
+    ],
+    include_package_data=True,
+    zip_safe=False
 )
