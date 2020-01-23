1f6eca6d71c7b78d3cc626d79866a869eb9bb1b2
Steven Herbst <sherbst@stanford.edu>
2020-01-22 17:24:06 -0800

    using more realistic channel model

--- tests/common.py
+++ tests/common.py
@@ -10,20 +10,37 @@ def adapt_fir(pack_dir = ".", config="system"):
     iterations  = 200000
     depth = 1000
 
-    # Cursor position with the most energy 
+    # Channel parameters
+    chan_type = 'exponent'
+    chan_tau = 2.0
+    chan_sampl_rate = 5
+    chan_resp_depth = 50
     pos = 2
-    resp_len = 125
 
     ideal_codes = np.random.randint(2, size=iterations)*2 - 1
 
-    chan = Channel(channel_type='perfect', normal='area', sampl_rate=5, cursor_pos=pos, resp_depth=resp_len)
-    chan_out = chan(ideal_codes)
+    chan = Channel(channel_type=chan_type,
+                   normal='area',
+                   tau=chan_tau,
+                   sampl_rate=chan_sampl_rate,
+                   resp_depth=chan_resp_depth,
+                   cursor_pos=pos)
+    chan_out = chan(ideal_codes) / chan_sampl_rate
+    # print(chan.impulse_response)
 
     # Adapt to the channel
     adapt = Wiener(step_size = ffe_config["adaptation"]["args"]["mu"], num_taps = ffe_config['parameters']["length"], cursor_pos=pos)
     for i in range(iterations-pos):
         adapt.find_weights_pulse(ideal_codes[i-pos], chan_out[i])    
     weights = adapt.weights
+
+    # Uncomment this line for debugging purposes to
+    # use weights corresponding to a perfect channel
+    # weights = np.array([1.0, 0, 0])
+
+    # Normalize weights
+    weights = weights * (100.0 / np.sum(np.abs(weights)))
+    # print(weights)
 
     # Quantize the weights
     qw = Quantizer(width=ffe_config["parameters"]["weight_precision"], signed=True)
@@ -37,14 +54,28 @@ def adapt_fir(pack_dir = ".", config="system"):
     generic_packager.create_package()
     ffe_packager.create_package()
 
+    # Write impulse response to package
+    impulse_length = len(chan.impulse_response)
+    impulse_values = ', '.join(f'{elem / chan_sampl_rate}'
+                               for elem in chan.impulse_response)
+    impulse_pack = pack_dir + '/' + 'impulse_pack.sv' 
+    with open(impulse_pack, "w+") as f:
+        f.write(f'''\
+package impulse_pack;
+    parameter integer impulse_length = {impulse_length};
+    parameter real impulse_values [{impulse_length}] = '{{{impulse_values}}};
+endpackage\
+''')
+
     # Write weights to package
-    with open(pack_dir + '/' + 'weights_pack.sv', "w+") as f:
+    weights_pack = pack_dir + '/' + 'weights_pack.sv'
+    with open(weights_pack, "w+") as f:
         f.write('package weights_pack;\n')
         f.write('logic signed [' + str(ffe_config["parameters"]["weight_precision"]) + '-1:0]  read_weights   [0:' + str(len(quantized_weights)) + '-1] = ')
         f.write('{' + ",".join([str(int(w)) for w in weights]) + '};\n')
         f.write('endpackage')
 
     # Return packages  
-    packages  = [generic_packager.path, ffe_packager.path, pack_dir + '/weights_pack.sv']
+    packages  = [generic_packager.path, ffe_packager.path, impulse_pack, weights_pack]
     return packages
 
