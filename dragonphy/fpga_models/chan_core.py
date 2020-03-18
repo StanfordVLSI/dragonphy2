9df2a32546075fa91f8c8b9f2b2e16175370ebe7
Steven Herbst <sherbst@stanford.edu>
2020-03-18 15:50:28 -0700

    added new inputs for channel

--- dragonphy/fpga_models/chan_core.py
+++ dragonphy/fpga_models/chan_core.py
@@ -22,6 +22,9 @@ def main():
     m = MixedSignalModel(Path(__file__).stem, dt=a.dt)
     m.add_analog_input('in_')
     m.add_analog_output('out')
+    m.add_analog_input('dt')
+    m.add_digital_input('clk')
+    m.add_digital_input('rst')
 
     # define model behavior
     m.set_this_cycle(m.out, m.in_)
@@ -35,3 +38,52 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# // update memory
+# always @(data_ana_i.value) begin
+#     // shift back memory
+#     for (int i=(mem_len-2); i>=0; i=i-1) begin
+#         t_mem[i+1]=t_mem[i];
+#         v_mem[i+1]=v_mem[i];
+#     end
+#
+#     // add new values at index 0
+#     t_mem[0] = $realtime;
+#     v_mem[0] = data_ana_i.value;
+# end
+#
+# // function to evaluate the step response at specific times
+# import step_resp_pack::*;
+# function real eval_at(input real t);
+#     integer idx;
+#     real weight;
+#     idx = $floor(t/step_dt);
+#     if (idx < 0) begin
+#         eval_at = v_step[0];
+#     end else if (idx >= (step_len-1)) begin
+#         eval_at = v_step[step_len-1];
+#     end else begin
+#         weight = (t/step_dt) - idx;
+#         eval_at = (1-weight)*v_step[idx] + weight*v_step[idx+1];
+#     end
+# endfunction
+#
+# // update the output of the transmitter
+# real tmp;
+# always @(data_ana_o.req) begin
+#     // calculate output
+#     tmp = 0;
+#     for (int i=0; i<(mem_len-1); i=i+1) begin
+#         if (i==0) begin
+#             tmp += v_mem[i]*eval_at($realtime-t_mem[i]);
+#         end else begin
+#             tmp += v_mem[i]*(eval_at($realtime-t_mem[i])-eval_at($realtime-t_mem[i-1]));
+#         end
+#     end
+#
+#     // assign to the output
+#     data_ana_o.value = tmp;
+#
+#     // acknowledge request
+#     ->>data_ana_o.ack;
+# end
--- vlog/fpga_models/chan.sv
+++ vlog/fpga_models/chan.sv
@@ -4,13 +4,29 @@ module chan (
     `ANALOG_INPUT data_ana_i,
     `ANALOG_OUTPUT data_ana_o
 );
+    // signals use for external I/O
+    (* dont_touch = "true" *) logic __emu_rst;
+    (* dont_touch = "true" *) logic __emu_clk;
+    (* dont_touch = "true" *) logic signed [((`DT_WIDTH)-1):0] __emu_dt;
+    (* dont_touch = "true" *) logic signed [((`DT_WIDTH)-1):0] __emu_dt_req;
+
+    // don't use __emu_dt_req, just set it to the highest value possible
+    assign __emu_dt_req = {1'b0, {((`DT_WIDTH)-1){1'b1}}};
+
+    // declare format for timestep
+    `REAL_FROM_WIDTH_EXP(DT_FMT, `DT_WIDTH, `DT_EXPONENT);
+
     generate
         chan_core #(
             `INTF_PASS_REAL(in_, data_ana_i.value),
-            `INTF_PASS_REAL(out, data_ana_o.value)
+            `INTF_PASS_REAL(out, data_ana_o.value),
+            `PASS_REAL(dt, DT_FMT)
         ) chan_core_i (
             .in_(data_ana_i.value),
-            .out(data_ana_o.value)
+            .out(data_ana_o.value),
+            .dt(__emu_dt),
+            .clk(__emu_clk),
+            .rst(__emu_rst)
         );
     endgenerate
 endmodule
