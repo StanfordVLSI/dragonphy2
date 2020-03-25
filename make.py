7cfc31ab0d88196009e34a14d396e79aed9b8fd0
zamyers <zmagnitar@gmail.com>
2020-03-25 10:51:04 -0700

    Switch from system.yml to loopback_config.yml

--- make.py
+++ make.py
@@ -12,10 +12,10 @@ def create_fpga_graph():
     graph.add_config('osc_model', folders=['config', 'fpga'])
     graph.add_config('rx_adc', folders=['config', 'fpga'])
     graph.add_config('tx', folders=['config', 'fpga'])
-    graph.add_config('system', folders=['config'])
+    graph.add_config('test_loopback_config', folders=['config'])
 
     #Add msdsl scripts to build list
-    graph.add_python('adapt_fir',       'adapt_fir',        'AdaptFir',     folders=['dragonphy'],                configs={'system'})
+    graph.add_python('adapt_fir',       'adapt_fir',        'AdaptFir',     folders=['dragonphy'],                configs={'test_loopback_config'})
     graph.add_python('chan_core',       'chan_core',        'ChannelCore',  folders=['dragonphy', 'fpga_models'], sources={'adapt_fir'}, configs={'chan'})
     graph.add_python('osc_model_core',  'osc_model_core',   'OscModelCore', folders=['dragonphy', 'fpga_models'], configs={'osc_model'})
     graph.add_python('rx_adc_core',     'rx_adc_core',      'RXAdcCore',    folders=['dragonphy', 'fpga_models'], configs={'rx_adc'})
