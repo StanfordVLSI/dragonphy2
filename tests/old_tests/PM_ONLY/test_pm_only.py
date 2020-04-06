21874b653adb9e57e6e9fc2323620f77a2af84cf
Steven Herbst <sherbst@stanford.edu>
2020-04-05 19:13:50 -0700

    temporarily comment out test_pm_only

--- tests/old_tests/PM_ONLY/test_pm_only.py
+++ tests/old_tests/PM_ONLY/test_pm_only.py
@@ -1,86 +1,86 @@
-# general imports
-import os
-from pathlib import Path
-import matplotlib.pyplot as plt
-import numpy as np
-
-# DragonPHY imports
-from dragonphy import *
-
-THIS_DIR = Path(__file__).parent.resolve()
-BUILD_DIR = THIS_DIR / 'build'
-if 'FPGA_SERVER' in os.environ:
-    SIMULATOR = 'vivado'
-else:
-    SIMULATOR = 'ncsim'
-
-
-CLK_ASYNC_FREQ = 501e6
-CLK_REF_FREQ = 4e9
-N_PM = 20
-T_TOL = 1e-12
-
-
-def plot_data(delay, pm):
-    plt.plot(delay*1e12, pm*1e12, '*')
-    plt.plot(delay*1e12, ((1/CLK_REF_FREQ)-pm)*1e12, '*')
-    plt.xlabel('True Delay (ps)')
-    plt.ylabel('Phase Monitor Estimate (ps)')
-    plt.legend(['Test', 'Tref-Test'])
-
-    plt.tight_layout()
-    plt.savefig('pm.eps')
-
-    plt.cla()
-    plt.clf()
-
-
-def check_data(delay, pm):
-    for t_true, t_est in zip(delay, pm):
-        # there is a 180 degree ambiguity in the measurement
-        delta_1 = abs(t_true - t_est)
-        delta_2 = abs(t_true - ((1/CLK_REF_FREQ) - t_est))
-        delta = min(delta_1, delta_2)
-        print(delta_1, delta_2)
-
-        assert delta <= T_TOL, f'Phase measurement error of {delta*1e12} ps is out of spec.'
-
-
-def test_sim():
-    def qwrap(s):
-        return f'"{s}"'
-    defines = {
-        'PM_TXT': qwrap(BUILD_DIR / 'pm.txt'),
-        'DELAY_TXT': qwrap(BUILD_DIR / 'delay.txt'),
-        'CLK_ASYNC_FREQ': CLK_ASYNC_FREQ,
-        'CLK_REF_FREQ': CLK_REF_FREQ,
-        'DAVE_TIMEUNIT': '1fs',
-        'NCVLOG': None
-    }
-
-    deps = get_deps_cpu_sim_old(impl_file=THIS_DIR / 'test.sv')
-    print(deps)
-
-    DragonTester(
-        ext_srcs=deps,
-        directory=BUILD_DIR,
-        top_module='test',
-        inc_dirs=[get_mlingua_dir() / 'samples', get_dir('inc/old_cpu')],
-        defines=defines,
-        simulator=SIMULATOR
-    ).run()
-
-    # read true delay
-    # delay = np.array(read_real('delay.txt'), dtype=float)
-    # delay = None
-
-    # get PM estimate of delay
-    # pm = np.array(read_logic('pm.txt'), dtype=float)
-    # pm *= (1.0/CLK_ASYNC_FREQ)/(2**(N_PM+1))
-    # pm = None
-
-    # plot results
-    # plot_data(delay=delay, pm=pm)
-
-    # check the results
-    # check_data(delay=delay, pm=pm)
+# # general imports
+# import os
+# from pathlib import Path
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# # DragonPHY imports
+# from dragonphy import *
+#
+# THIS_DIR = Path(__file__).parent.resolve()
+# BUILD_DIR = THIS_DIR / 'build'
+# if 'FPGA_SERVER' in os.environ:
+#     SIMULATOR = 'vivado'
+# else:
+#     SIMULATOR = 'ncsim'
+#
+#
+# CLK_ASYNC_FREQ = 501e6
+# CLK_REF_FREQ = 4e9
+# N_PM = 20
+# T_TOL = 1e-12
+#
+#
+# def plot_data(delay, pm):
+#     plt.plot(delay*1e12, pm*1e12, '*')
+#     plt.plot(delay*1e12, ((1/CLK_REF_FREQ)-pm)*1e12, '*')
+#     plt.xlabel('True Delay (ps)')
+#     plt.ylabel('Phase Monitor Estimate (ps)')
+#     plt.legend(['Test', 'Tref-Test'])
+#
+#     plt.tight_layout()
+#     plt.savefig('pm.eps')
+#
+#     plt.cla()
+#     plt.clf()
+#
+#
+# def check_data(delay, pm):
+#     for t_true, t_est in zip(delay, pm):
+#         # there is a 180 degree ambiguity in the measurement
+#         delta_1 = abs(t_true - t_est)
+#         delta_2 = abs(t_true - ((1/CLK_REF_FREQ) - t_est))
+#         delta = min(delta_1, delta_2)
+#         print(delta_1, delta_2)
+#
+#         assert delta <= T_TOL, f'Phase measurement error of {delta*1e12} ps is out of spec.'
+#
+#
+# def test_sim():
+#     def qwrap(s):
+#         return f'"{s}"'
+#     defines = {
+#         'PM_TXT': qwrap(BUILD_DIR / 'pm.txt'),
+#         'DELAY_TXT': qwrap(BUILD_DIR / 'delay.txt'),
+#         'CLK_ASYNC_FREQ': CLK_ASYNC_FREQ,
+#         'CLK_REF_FREQ': CLK_REF_FREQ,
+#         'DAVE_TIMEUNIT': '1fs',
+#         'NCVLOG': None
+#     }
+#
+#     deps = get_deps_cpu_sim_old(impl_file=THIS_DIR / 'test.sv')
+#     print(deps)
+#
+#     DragonTester(
+#         ext_srcs=deps,
+#         directory=BUILD_DIR,
+#         top_module='test',
+#         inc_dirs=[get_mlingua_dir() / 'samples', get_dir('inc/old_cpu')],
+#         defines=defines,
+#         simulator=SIMULATOR
+#     ).run()
+#
+#     # read true delay
+#     # delay = np.array(read_real('delay.txt'), dtype=float)
+#     # delay = None
+#
+#     # get PM estimate of delay
+#     # pm = np.array(read_logic('pm.txt'), dtype=float)
+#     # pm *= (1.0/CLK_ASYNC_FREQ)/(2**(N_PM+1))
+#     # pm = None
+#
+#     # plot results
+#     # plot_data(delay=delay, pm=pm)
+#
+#     # check the results
+#     # check_data(delay=delay, pm=pm)
