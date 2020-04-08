bc0abe8caec85e90380def02a668bb2c3aff63dd
Steven Herbst <sherbst@stanford.edu>
2020-04-08 15:53:57 -0700

    input buffer example working

--- vlog/new_cpu_models/digital_core/input_buffer_inv.sv
+++ vlog/new_cpu_models/digital_core/input_buffer_inv.sv
@@ -1,21 +1,10 @@
-`default_nettype none
- 
-
-module input_buffer_inv ( 
-	input wire logic inp, 
-	input wire logic inm, 
-	input wire logic pd, 
-	output wire clk, 
-	output wire clk_b
+module input_buffer_inv (
+    input wire logic inp,
+    input wire logic inm,
+    input wire logic pd,
+    output wire logic clk,
+    output wire logic clk_b
 );
-
-//synopsys translate_off
-
-assign clk = pd ? 1'b0 : inp;
-assign clk_b = pd ? 1'b0 : inm;
-
-//synopsys translate_on
-
+    assign clk = pd ? 1'b0 : inp;
+    assign clk_b = pd ? 1'b0 : inm;
 endmodule
-`default_nettype wire
-
