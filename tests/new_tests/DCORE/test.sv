e4c01669078d04d73199489a32df9749f657f7fd
Steven Herbst <sherbst@stanford.edu>
2020-04-08 19:16:04 -0700

    don't dump waveforms as the default

--- tests/new_tests/DCORE/test.sv
+++ tests/new_tests/DCORE/test.sv
@@ -1,21 +1,4 @@
-`include "mLingua_pwl.vh"
-
 module test;
-	import const_pack::*;
-	import test_pack::*;
-
-	logic ext_clkp;
-	logic ext_clkn;
-
-	clock #(
-		.freq(full_rate/2),
-		.duty(0.5),
-		.td(0)
-	) iEXTCLK (
-		.ckout(ext_clkp),
-		.ckoutb(ext_clkn)
-	);
-
     acore_debug_intf ad_intf_i();
     jtag_intf jtag_intf_i();
 
