f63aaa6a75b5d5c8972e2ce4025e9f55018b2074
Steven Herbst <sherbst@stanford.edu>
2020-04-10 17:27:15 -0700

    add facility for work in progress tests

--- conftest.py
+++ conftest.py
@@ -7,17 +7,22 @@ def pytest_addoption(parser):
     parser.addoption(
         "--runslow", action="store_true", default=False, help="run slow tests"
     )
+    
+    parser.addoption(
+        "--runwip", action="store_true", default=False, help="run tests that are currently a work in progress"
+    )
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-
+    config.addinivalue_line("markers", "wip: mark test as work-in-progress")
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
     skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_wip = pytest.mark.skip(reason="need --runwip option to run")
+    
     for item in items:
-        if "slow" in item.keywords:
+        if config.getoption("--runslow") and "slow" in item.keywords:
             item.add_marker(skip_slow)
+        elif config.getoption("--runwip") and "wip" in item.keywords:
+            item.add_marker(skip_wip)
