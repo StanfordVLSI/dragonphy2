6ef33e644e5a2cb392415a80e9f2d3f3479dd62c
Steven Herbst <sherbst@stanford.edu>
2020-04-13 14:38:20 -0700

    DC simulation runs but is not producing the right results

--- tests/new_tests/DC/test.sv
+++ tests/new_tests/DC/test.sv
@@ -1,146 +1,154 @@
 `include "mLingua_pwl.vh"
-`include "iotype.sv"
-`default_nettype none
+
+`define FORCE_ADBG(name, value) force top_i.iacore.adbg_intf_i.``name`` = ``value``
+`define FORCE_DDBG(name, value) force top_i.idcore.ddbg_intf_i.``name`` = ``value``
+
+`ifndef RX_INPUT_TXT
+    `define RX_INPUT_TXT
+`endif
+
+`ifndef TI_ADC_TXT
+    `define TI_ADC_TXT
+`endif
 
 module test;
-
-	import const_pack::*;
 	import test_pack::*;
 	import checker_pack::*;
-	import jtag_reg_pack::*;
+
+	// clock inputs
+
+	logic ext_clkp;
+	logic ext_clkn;
+
+	// reset
+
+	logic rstb;
+
+	// JTAG driver
+
+	jtag_intf jtag_intf_i ();
+	jtag_drv jtag_drv_i (jtag_intf_i);
+
+    // stimulus parameters
 
 	localparam real v_diff_min = -0.4;
 	localparam real v_diff_max = +0.4;
 	localparam real v_diff_step = 0.01;
-	localparam `real_t v_cm = 0.40;
+	localparam real v_cm = 0.40;
 
 	// mLingua initialization
+
 	PWLMethod pm=new;
 
 	// Analog inputs
-	`pwl_t ch_outp;
-	`pwl_t ch_outn;
-	//`real_t v_cm;
-    `voltage_t v_cal;
 
-	// clock inputs 
-	logic clk_async;
-	logic clk_jm_p;
-	logic clk_jm_n;
-	logic ext_clkp;
-	logic ext_clkn;
-    logic signed [Nadc-1:0] adcout_conv_signed [Nti-1:0];
-	// clock outputs
-	logic clk_out_p;
-	logic clk_out_n;
-	logic clk_trig_p;
-	logic clk_trig_n;
-
-	logic clk_slow;
-    logic rstb;
-	// dump control
-	logic dump_start;
-	logic clk_cdr;
-	// JTAG
-	jtag_intf jtag_intf_i();
-
+	pwl ch_outp;
+	pwl ch_outn;
 
 	// instantiate top module
-	butterphy_top top_i (
-		// analog inputs
+
+	dragonphy_top top_i (
+	    // analog inputs
 		.ext_rx_inp(ch_outp),
 		.ext_rx_inn(ch_outn),
 		.ext_Vcm(v_cm),
-		.ext_Vcal(v_cal),
+	    .ext_Vcal(0.6),
 
-		// clock inputs 
+		// clock inputs
 		.ext_clkp(ext_clkp),
 		.ext_clkn(ext_clkn),
 
-		// clock outputs
-		.clk_out_p(clk_out_p),
-		.clk_out_n(clk_out_n),
-		.clk_trig_p(clk_trig_p),
-		.clk_trig_n(clk_trig_n),
-		// dump control
-		.ext_dump_start(dump_start),
+        // reset
         .ext_rstb(rstb),
-		// JTAG
+
+        // JTAG
 		.jtag_intf_i(jtag_intf_i)
+		// other I/O not used..
 	);
 
+	// External clock
+
+    localparam real ext_clk_freq = full_rate/2;
 	clock #(
-		.freq(full_rate/2), //Depends on divider !
+		.freq(ext_clk_freq),
 		.duty(0.5),
 		.td(0)
 	) iEXTCLK (
 		.ckout(ext_clkp),
 		.ckoutb(ext_clkn)
-	); 
+	);
 
-	jtag_drv jtag_drv_i (jtag_intf_i);
-	// Force CDR Clock
+    // Data recording
 
-	// RSTB
+    logic record;
 
-	// Save signals for post-processing
-
-	logic record;
-
-	rx_input_recorder rx_input_recorder_i (
+    rx_input_recorder #(
+        .filename(`RX_INPUT_TXT)
+    ) rx_input_recorder_i (
 		.in_p(ch_outp),
 		.in_n(ch_outn),
 		.clk(record),
 		.en(1'b1)
 	);
-	
-    ti_adc_recorder ti_adc_recorder_i (
+
+    ti_adc_recorder #(
+        .filename(`TI_ADC_TXT)
+    ) ti_adc_recorder_i (
 		.in(top_i.idcore.adcout_unfolded[15:0]),
 		.clk(record),
 		.en(1'b1)
 	);
 
-	// Main stimulus program
+	// Main test
 
 	initial begin
-		//force top_i.idcore.iMM_CDR.iMM_AVG_IIR.in = +'sd383;
-		record = 1'b0;
-		rstb = 1'b1;
-		#(20ns);
+		// Initialize pins
+		$display("Initializing pins...");
+		jtag_drv_i.init();
+
+		// Toggle reset
+		$display("Toggling reset...");
+        #(20ns);
 		rstb = 1'b0;
 		#(20ns);
 		rstb = 1'b1;
-		#(10ns);
-
-		// Initialize JTAG
-		jtag_drv_i.init();
 
 		// Enable the input buffer
-		jtag_drv_i.write_tc_reg(en_inbuf, 'b1);
-		jtag_drv_i.write_tc_reg(en_v2t, 'b1);
-		ch_outp = pm.write(v_cm, 0, 0);
-		ch_outn = pm.write(v_cm, 0, 0);
-		jtag_drv_i.write_tc_reg(int_rstb, 'b1);
+		$display("Set up the input buffer...");
+		`FORCE_ADBG(en_gf, 1);
+        #(1ns);
+		`FORCE_ADBG(bypass_inbuf_div, 0);
+		#(1ns);
+        `FORCE_ADBG(en_inbuf, 1);
+        #(1ns);
+        `FORCE_ADBG(en_v2t, 1);
+        #(1ns);
+        `FORCE_DDBG(int_rstb, 1);
+        #(1ns);
 
-		//repeat (2) @(top_i.idcore.clk_cdr);
-		//force top_i.idcore.cdr_rstb = 1;
-		
-		for (real v_diff = v_diff_min; v_diff <= v_diff_max+v_diff_step; v_diff = v_diff+v_diff_step) begin
+		// Wait a little bit to measure frequencies
+		#(100ns);
+
+        // Walk through differential input voltages
+		for (real v_diff = v_diff_min;
+		     v_diff <= v_diff_max + v_diff_step;
+		     v_diff = v_diff + v_diff_step
+		) begin
 			ch_outp = pm.write(v_cm+v_diff/2.0, 0, 0);
 			ch_outn = pm.write(v_cm-v_diff/2.0, 0, 0);
-			
+
 			$display("Differential input: %0.3f V", ch_outp.a-ch_outn.a);
-			
+
 			#(15ns);
 
 			record = 1'b1;
-			#0;
+			#(1ns);
 			record = 1'b0;
+		    #(1ns);
 		end
 
 		$finish;
 	end
-
 endmodule
 
-`default_nettype wire
+
