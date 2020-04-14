66cb933b8eaa64bfed4913935157476c771ffc14
zamyers <zmagnitar@gmail.com>
2020-04-14 14:51:30 -0700

    fixing PD issues

--- vlog/chip_src/digital_pd.sv
+++ vlog/chip_src/digital_pd.sv
@@ -13,43 +13,47 @@ module digital_pd #(
     typedef enum logic [1:0] {COUNT_UP, RESET, COUNT_DOWN} pd_state_t;
     pd_state_t pd_state;
 
-    always_ff @(posedge clk or posedge clk_p or posedge clk_n or negedge rstb) begin
+
+    always_ff @(posedge clk_p or posedge clk_n or negedge rstb) begin
         if(~rstb) begin
             pd_state <= RESET;
-            pd_out <= 0;
         end else begin
             case (pd_state)
                 RESET : begin
                     if (clk_p) begin
-                        pd_out <= 1;
                         pd_state <= COUNT_UP;
                     end else if(clk_n) begin
-                        pd_out <= -1;
                         pd_state <= COUNT_DOWN;
-                    end else begin
-                        pd_out <= 0;
-                        pd_state <= RESET;
                     end
                 end
                 COUNT_UP : begin
                     if(clk_n) begin
-                        pd_out <= 0;
                         pd_state <= RESET
                     end else begin
-                        pd_out <= pd_out + 1;
                         pd_state <= COUNT_UP;
                     end
                 end
                 COUNT_DOWN: begin
                     if(clk_p) begin
-                        pd_out <= 0;
                         pd_state <= RESET
                     end else begin
-                        pd_out <= pd_out - 1;
                         pd_state <= COUNT_DOWN;
                     end
                 end
             endcase
         end
     end
+
+    always_ff @(posedge clk or negedge rstb) begin
+        if(~rstb) begin
+            pd_out <= 0;
+        end else begin
+            case (pd_state)
+                RESET : pd_out <= 0;
+                COUNT_UP : pd_out <= pd_out + 1;
+                COUNT_DOWN : pd_out <= pd_out -1;
+            endcase
+        end
+    end
+
 endmodule : digital_pd
