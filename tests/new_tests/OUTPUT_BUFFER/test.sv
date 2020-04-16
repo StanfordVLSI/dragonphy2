324538fa3ad8ba8eb3eabdd2a05179dd98cf3175
Sung-Jin Kim <sjkim85@stanford.edu>
2020-04-16 00:54:53 -0700

    update analog_core issue #55 and #56 trial16

--- tests/new_tests/OUTPUT_BUFFER/test.sv
+++ tests/new_tests/OUTPUT_BUFFER/test.sv
@@ -118,8 +118,6 @@ module test;
 		$display("Set up the input buffer...");
 		`FORCE_ADBG(en_gf, 1);
         #(1ns);
-		`FORCE_ADBG(bypass_inbuf_div, 0);
-		#(1ns);
         `FORCE_ADBG(en_inbuf, 1);
         #(1ns);
         `FORCE_ADBG(en_v2t, 1);
