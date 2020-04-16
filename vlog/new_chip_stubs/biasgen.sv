c2ef145318e33482012458e869416f6ac062aece
Sung-Jin Kim <sjkim85@stanford.edu>
2020-04-16 05:19:33 -0700

    update analog_core issue #55 and #56 trial21

--- vlog/new_chip_stubs/biasgen.sv
+++
@@ -1,6 +0,0 @@
-module biasgen import const_pack::Nbias; (
-    input en,
-    input [Nbias-1:0] ctl,
-    input Vbias
-);
-endmodule
---
+++ vlog/new_chip_stubs/ff_e_cn_rn.sv
@@ -0,0 +1,8 @@
+module ff_e_c_rn(
+    input D,
+    input E,
+    input CP,
+    output Q,
+    input CDN,
+);
+endmodule
