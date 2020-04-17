2f6ddd4bacb454404ec11a4d85b1be529e37ba70
Zachary Myers <zmagnitar@gmail.com>
2020-04-17 14:40:46 -0700

    I dont know why this error is now happening??

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires_list = [
     'matplotlib',
     'sklearn',
     'pygraphviz',
-    'html5lib',
+    'lxml',
     'justag==0.0.2.9',
     # general requirements with special versions to prevent
     # warnings that clutter pytest output
