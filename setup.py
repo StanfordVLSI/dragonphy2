25b5ac854cdda3e6859f039dba56d2981f12a64e
Zachary Myers <zmagnitar@gmail.com>
2020-04-17 14:38:00 -0700

    small syntax error :(

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires_list = [
     'matplotlib',
     'sklearn',
     'pygraphviz',
-    'html5lib'
+    'html5lib',
     'justag==0.0.2.9',
     # general requirements with special versions to prevent
     # warnings that clutter pytest output
