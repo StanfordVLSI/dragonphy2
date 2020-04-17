0fded12828ca48f13c1ddb4ff161fec61f785009
Zachary Myers <zmagnitar@gmail.com>
2020-04-17 14:10:17 -0700

    Going to check this in and move onto working on the CDR

--- tests/new_tests/SRAM/test.sv
+++ tests/new_tests/SRAM/test.sv
@@ -12,7 +12,7 @@ module test;
 
     import const_pack::*;
 
-    localparam real freq = 1e9;
+    localparam real freq = 4e9;
     localparam integer N_mem_tiles = 4;
     localparam integer Nwrite = (2**(N_mem_addr+$clog2(N_mem_tiles)))*2;    // write more data than SRAM can hold
                                                        // to make sure we capture just the beginning
@@ -24,12 +24,15 @@ module test;
 
     logic rstb;
     logic clk;
+    logic clk_r;
+    logic pulse_write;
     logic start;
     logic [N_mem_addr+$clog2(N_mem_tiles)-1:0] addr;
 
     // instantiate the memory
     initial begin
-        $shm_open("waves.shm"); $shm_probe("AS");
+        $shm_open("waves.shm"); $shm_probe("ACT");
+        $shm_probe(out); $shm_probe(in);
     end
     oneshot_multimemory #(.N_mem_tiles(4))  oneshot_memory_i (
         .clk(clk),
@@ -48,7 +51,7 @@ module test;
         .filename(`SRAM_IN_TXT)
     ) sram_in_recorder (
         .in(in),
-        .clk(clk),
+        .clk(clk_r),
         .en(in_record)
     );
 
@@ -56,12 +59,17 @@ module test;
         .filename(`SRAM_OUT_TXT)
     ) sram_out_recorder (
         .in(out),
-        .clk(clk),
+        .clk(pulse_write),
         .en(out_record)
     );
 
     // generate the clock
 
+    initial begin 
+        clk_r = 1'b0;
+        forever `WAIT(0.5)  clk_r = ~clk_r;
+    end
+    
     always begin
         clk = 1'b0;
         `WAIT(0.5);
@@ -118,28 +126,29 @@ module test;
         in_record = 1'b0;
 
         // wait a bit so that we're definitely in the WRITE_DONE state
-
+        
         `WAIT(10.0);
 
         // go to the WAIT_FOR_WRITE state
 
         start = 1'b0;
-        `WAIT(1.0);
-
-        // clock in the first memory address
-
         addr = 0;
         `WAIT(1.0);
+        addr = 1;
+        // clock in the first memory address
 
+        `WAIT(0.1);
         // read out the memory contents
-
         out_record = 1'b1;
-
-        for (int j=1; j<Nread; j=j+1) begin
+        `WAIT(0.9);
+        out_record = 1'b0;
+        for (int j=2; j<Nread; j=j+1) begin
             addr = j;
-            `WAIT(1.0);
+            `WAIT(0.1);
+            out_record = 1'b1;
+            `WAIT(0.9);
+            out_record = 1'b0;
         end
-
         `WAIT(1.0);
 
         $finish;
