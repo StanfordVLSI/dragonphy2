3b6d3127165c45bd57f9c2f5bde78bba31db04f3
Steven Herbst <sherbst@stanford.edu>
2020-04-22 15:15:57 -0700

    add test for 1-bit phase blender and update model

--- vlog/new_cpu_models/analog_core/phase_blender_1b.sv
+++ vlog/new_cpu_models/analog_core/phase_blender_1b.sv
@@ -14,70 +14,92 @@ Todo:
 module phase_blender_1b (
     input wire logic [1:0] ph_in,    // clocks being interpolated
     input wire logic en_mixer,       // '1': phase blend, '0': bypass leading clock
-    output reg ph_out                // blended phase
+    output reg ph_out=1'b0           // blended phase
 );
 
-timeunit 1fs;
-timeprecision 1fs;
+    timeunit 1fs;
+    timeprecision 1fs;
 
-import model_pack::*;
+    import model_pack::PIParameter;
 
-//----- SIGNAL DECLARATION -----
-real t_lead;
-real tdiff;                  // time difference b/t two clock phases
-real tout;                   // phase interpolated delay
-real wgt;                    // phase interpolation weight
-real td, rj;                 // delay of this cell, random jitter
-logic sign;                  // indicates which clock leads the other
-logic ph_lead;               // selected clock that leads the other
-logic ph_in0_d, ph_in1_d;    // delayed signal of ph_in[0], ph_in[1]
-
-//----- FUNCTIONAL DESCRIPTION -----
-
-// design parameter class init
-PIParameter pi_obj;
-
-initial begin
-    pi_obj = new();
-    td = pi_obj.td_mixer1b;
-end
-
-// phase interpolation weight
-assign wgt = en_mixer ? 0.5 : 0.0;
-
-// delay two inputs
-always @(ph_in[0]) begin
-    ph_in0_d <= #(td*1s) ph_in[0];
-end
-always @(ph_in[1]) begin
-    ph_in1_d <= #(td*1s) ph_in[1];
-end
-
-// find which clock input leads
-// TODO: why is this assignment non-blocking?
-always @(ph_in[0]) begin
-    sign <= ph_in[0] ^ ph_in[1];
-end
-assign ph_lead = sign ? ph_in0_d : ph_in1_d;
-
-// compute the phase difference
-always @(ph_in) begin
-    if (ph_in[0] ^ ph_in[1]) begin
-        t_lead = ($realtime/1s);
-    end else begin
-        tdiff = ($realtime/1s) - t_lead;
-        tout = tdiff * wgt;
+    // design parameter class init
+    PIParameter pi_obj;
+    real td;
+    initial begin
+        pi_obj = new();
+        td = pi_obj.td_mixer1b;
     end
-end
 
-// phase interpolation
-always @(ph_lead) begin
-    rj = pi_obj.get_rj_mixer1b();
-    if (sign) begin
-        ph_out <= #((tout+rj)*1s) ph_in0_d;
-    end else begin
-        ph_out <= #((tdiff-tout+rj)*1s) ph_in1_d;
+    // phase interpolation weight
+    real wgt;
+    assign wgt = en_mixer ? 0.5 : 0.0;
+
+    // state variables
+    logic nxt_state=1'b0;
+    real rise0=-1;
+    real fall0=-1;
+    real rise1=-1;
+    real fall1=-1;
+
+    real ttotr0;
+    always @(posedge ph_in[0]) begin
+        if (nxt_state != 1'b1) begin
+            // update nxt_state
+            nxt_state = 1'b1;
+            // calculate delay
+            if ((rise0 != -1) && (rise1 != -1) && (rise1 >= rise0)) begin
+                ttotr0 = wgt*(rise1-rise0) + td + pi_obj.get_rj_mixer1b();
+            end else begin
+                ttotr0 = td + pi_obj.get_rj_mixer1b();
+            end
+            // schedule output
+            ph_out <= #(ttotr0*1s) 1'b1;
+        end
+        rise0 = $realtime/1s;
     end
-end
 
+    real ttotf0;
+    always @(negedge ph_in[0]) begin
+        if (nxt_state != 1'b0) begin
+            nxt_state = 1'b0;
+            if ((fall0 != -1) && (fall1 != -1) && (fall1 >= fall0)) begin
+                ttotf0 = wgt*(fall1-fall0) + td + pi_obj.get_rj_mixer1b();
+            end else begin
+                ttotf0 = td + pi_obj.get_rj_mixer1b();
+            end
+            ph_out <= #(ttotf0*1s) 1'b0;
+        end
+        fall0 = $realtime/1s;
+    end
+
+    real ttotr1;
+    always @(posedge ph_in[1]) begin
+        if (nxt_state != 1'b1) begin
+            // update nxt_state
+            nxt_state = 1'b1;
+            // calculate delay
+            if ((rise0 != -1) && (rise1 != -1) && (rise0 >= rise1)) begin
+                ttotr1 = (1.0-wgt)*(rise0-rise1) + td + pi_obj.get_rj_mixer1b();
+            end else begin
+                ttotr1 = td + pi_obj.get_rj_mixer1b();
+            end
+            // schedule output
+            ph_out <= #(ttotr1*1s) 1'b1;
+        end
+        rise1 = $realtime/1s;
+    end
+
+    real ttotf1;
+    always @(negedge ph_in[1]) begin
+        if (nxt_state != 1'b0) begin
+            nxt_state = 1'b0;
+            if ((fall0 != -1) && (fall1 != -1) && (fall0 >= fall1)) begin
+                ttotf1 = (1.0-wgt)*(fall0-fall1) + td + pi_obj.get_rj_mixer1b();
+            end else begin
+                ttotf1 = td + pi_obj.get_rj_mixer1b();
+            end
+            ph_out <= #(ttotf1*1s) 1'b0;
+        end
+        fall1 = $realtime/1s;
+    end
 endmodule
