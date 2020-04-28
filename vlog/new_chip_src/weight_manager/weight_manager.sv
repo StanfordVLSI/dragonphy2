fddc07ac37dc89dd573a67ff3a9bdd682cf5f9ab
zamyers <zmagnitar@gmail.com>
2020-04-28 14:59:08 -0700

    Speculative weight manager

--- vlog/new_chip_src/weight_manager/weight_manager.sv
+++ vlog/new_chip_src/weight_manager/weight_manager.sv
@@ -13,10 +13,14 @@ module weight_manager #(
     output logic signed [bitwidth-1:0] weights [width-1:0][depth-1:0]
 );
     
+    typedef enum logic [1:0] {READY, HALT} manager_state_t;
+    manager_state_t manager_state;
+
     logic is_increment;
     logic [$clog2(depth)-1:0] depth_addr;
     logic [$clog2(width)-1:0] width_addr;
     logic signed [bitwidth-1:0] increment_weights [width-1:0];
+    logic signed [bitwidth-1:0] set_weights[width-1:0];
     logic signed [bitwidth-1:0] next_weights [width-1:0];
 
 
@@ -25,21 +29,40 @@ module weight_manager #(
     assign width_addr   = inst[$clog2(width) + $clog2(depth)-1: $clog2(depth)]
 
     genvar gi;
-
     generate
         for(gi=0 ; gi < width; gi = gi + 1) begin
             assign increment_weights[gi] =  weights[gi][depth_addr] + $signed(data[2*gi+1:2*gi]);
-            assign next_weights[gi] = is_increment ? increment_weights[gi] :  
-
-            always_ff @(posedge clk or negedge rstb) begin
-                if(~rstb) begin
-                    manager_state <= READY;
-                end else begin
-                    weights <= exec ? next_weights : weights
-                end
-            end
+            assign next_weights[gi][depth_addr] = is_increment ? increment_weights[gi] : set_weights[gi];
         end
     endgenerate 
+
+    always_comb begin
+        int ii;
+        for(ii=0; ii<width; ii=ii+1) begin
+            if(ii == width_addr) begin
+                set_weights[ii] = $signed(data[bitwidth-1:0]);
+            end else begin
+                set_weights[ii] = weights[ii][depth_addr];
+            end
+        end
+    end
+
+    always_ff @(posedge clk or negedge rstb) begin
+        if(~rstb) begin
+           manager_state <= READY;
+        end else begin
+            case(manager_state)
+                READY: begin
+                    weights[depth_addr] <= exec ? next_weights : weights[depth_addr];
+                    manager_state <= exec ? HALT : READY;
+                end
+                HALT: begin
+                    manager_state <= exec ? HALT : READY;
+                end
+        end
+    end
+
+
 
 
 endmodule : weight_manager
