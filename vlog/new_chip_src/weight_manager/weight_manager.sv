70a91501a503ed95a43890b1110c80b25ce0a0d8
zamyers <zmagnitar@gmail.com>
2020-04-29 23:42:26 -0700

    Weight Manager Test Bench

--- vlog/new_chip_src/weight_manager/weight_manager.sv
+++ vlog/new_chip_src/weight_manager/weight_manager.sv
@@ -3,38 +3,34 @@ module weight_manager #(
     parameter integer depth=7,
     parameter integer bitwidth=8
 ) (
-    input wire logic [width*2-1:0] data,
-    input wire logic [1+$clog2(width)+$clog2(depth)-1:0] inst,
-    input wire logic exec,
+    input wire logic [width*2-1:0] data_reg,
+    input wire logic [1+$clog2(width)+$clog2(depth)-1:0] inst_reg,
+    input wire logic exec_reg,
 
     input wire logic clk,
     input wire logic rstb,
 
+    output logic signed [bitwidth-1:0] read_reg,
     output logic signed [bitwidth-1:0] weights [width-1:0][depth-1:0]
 );
     
-    typedef enum logic [1:0] {READY, HALT} manager_state_t;
+    typedef enum logic {READY, HALT} manager_state_t;
     manager_state_t manager_state;
 
     logic is_increment;
     logic [$clog2(depth)-1:0] depth_addr;
     logic [$clog2(width)-1:0] width_addr;
+    
     logic signed [bitwidth-1:0] increment_weights [width-1:0];
     logic signed [bitwidth-1:0] set_weights[width-1:0];
     logic signed [bitwidth-1:0] next_weights [width-1:0];
 
-
+    //Top bit indicates the instruction type
     assign is_increment = inst[1+$clog2(width)+$clog2(depth)-1];
-    assign depth_addr   = inst[$clog2(depth)-1:0];
-    assign width_addr   = inst[$clog2(width) + $clog2(depth)-1: $clog2(depth)]
 
-    genvar gi;
-    generate
-        for(gi=0 ; gi < width; gi = gi + 1) begin
-            assign increment_weights[gi] =  weights[gi][depth_addr] + $signed(data[2*gi+1:2*gi]);
-            assign next_weights[gi][depth_addr] = is_increment ? increment_weights[gi] : set_weights[gi];
-        end
-    endgenerate 
+    //The bottom part contains X Y location
+    assign depth_addr   = inst[$clog2(depth)-1:0]; 
+    assign width_addr   = inst[$clog2(width) + $clog2(depth)-1: $clog2(depth)]
 
     always_comb begin
         int ii;
@@ -47,22 +43,37 @@ module weight_manager #(
         end
     end
 
-    always_ff @(posedge clk or negedge rstb) begin
-        if(~rstb) begin
-           manager_state <= READY;
-        end else begin
-            case(manager_state)
-                READY: begin
-                    weights[depth_addr] <= exec ? next_weights : weights[depth_addr];
-                    manager_state <= exec ? HALT : READY;
-                end
-                HALT: begin
-                    manager_state <= exec ? HALT : READY;
-                end
+    genvar gi, gj;
+    generate
+        for(gi=0 ; gi < width; gi = gi + 1) begin
+            assign increment_weights[gi] =  weights[gi][depth_addr] + $signed(data[2*gi+1:2*gi]);
+            assign next_weights[gi][depth_addr] = is_increment ? increment_weights[gi] : set_weights[gi];
         end
-    end
 
 
-
+        always_ff @(posedge clk or negedge rstb) begin
+            if(~rstb) begin
+                manager_state <= READY;
+                for(gi=0 ; gi < width; gi = gi + 1) begin
+                    for(gj=0; gj < depth; gj = gj + 1) begin
+                        weights[gi][gj] <= 0;
+                    end
+                end
+                read_reg <= 0;
+            end else begin
+                case(manager_state)
+                    READY: begin
+                        for(gi=0 ; gi < width; gi = gi + 1) begin
+                            weights[gi][depth_addr] <= exec ? next_weights[gi] : weights[gi][depth_addr];
+                        end
+                        manager_state <= exec ? HALT : READY;
+                        read_reg      <= exec ? $signed(weights[width_addr][depth_addr]) : read_reg;
+                    end
+                    HALT: begin
+                        manager_state <= exec ? HALT : READY;
+                    end
+            end
+        end
+    endgenerate 
 
 endmodule : weight_manager
