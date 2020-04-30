37f3c2f2813fad6d23b0328ec2cb514163d79ed4
Zachary Myers <zmagnitar@gmail.com>
2020-04-30 14:02:50 -0700

    Trickery to get tasks to work properly

--- vlog/tb/wm/wm_tb.sv
+++ vlog/tb/wm/wm_tb.sv
@@ -7,6 +7,8 @@ module testbench;
     logic clk, rstb;
 
     logic [width*2-1:0] data_reg;
+    logic [width*2-1:0] d_reg_arr;
+    logic signed [1:0] arr [width-1:0];
     logic  [1+$clog2(width)+$clog2(depth)-1:0] inst_reg;
     logic exec = 0;
 
@@ -26,25 +28,39 @@ module testbench;
         .weights(weights)
     );
 
+    arr2dregconv #(.width(width)) adregc_i (.arr(arr), .d_reg(d_reg_arr));
+
+    genvar gj;
+    generate
+    for(gj=0; gj<width; gj=gj+1) begin
+        initial begin
+            arr[gj] = 0;
+        end
+    end
+    endgenerate
 
     initial begin
         rstb      = 0;
+        data_reg  = 0;
+        inst_reg  = 0; 
         #1ns rstb = 1;
 
         increment(0, {1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1});
-        load(0, 0, +8'd50)
-        load(1, 0, +8'd51)
-        load(2, 0, +8'd52)
+        load(0, 0, +8'd50);
+        load(1, 0, +8'd51);
+        load(2, 0, +8'd52);
     end
 
     task increment(input logic [$clog2(depth)-1:0] d_idx, input logic [1:0] inc_arr [width-1:0]);
-        int ii;
+        int gi;
         @(posedge clk) inst_reg[$clog2(depth)+$clog2(width)] = 1;
         inst_reg[$clog2(depth)+$clog2(width)-1:$clog2(depth)] = 0;
         inst_reg[$clog2(depth)-1:0] = d_idx;
-        for(ii=0; ii<width; ii = ii+1) begin
-            data_reg[2*ii+1:2*ii] = inc_arr[ii];
+        for(gi=0; gi<width;gi=gi+1) begin
+            arr[gi]      = inc_arr[gi];
         end
+        @(posedge clk) data_reg = d_reg_arr;
+        toggle_exec();
     endtask
 
     task load(input logic [$clog2(depth)-1:0] d_idx, logic [$clog2(width)-1:0] w_idx, logic [bitwidth-1:0] value);
@@ -52,6 +68,7 @@ module testbench;
         inst_reg[$clog2(depth)+$clog2(width)-1:$clog2(depth)] = w_idx;
         inst_reg[$clog2(depth)-1:0] = d_idx;
         data_reg[bitwidth-1:0] = value;
+        toggle_exec();
     endtask
 
     task toggle_exec;
@@ -61,6 +78,20 @@ module testbench;
 
 
 endmodule : testbench
+
+module arr2dregconv #(
+    parameter integer width=16
+)(
+    input logic signed [1:0] arr [width-1:0],
+    output logic [2*width-1:0] d_reg
+);
+    genvar gi;
+    generate
+        for(gi=0; gi<width; gi=gi+1) begin
+            assign d_reg[2*gi+1:2*gi] = arr[gi];
+        end
+    endgenerate
+endmodule 
 
 module clock #(
     parameter real delay=0ps,
@@ -74,7 +105,4 @@ module clock #(
         #delay
         forever begin #(period/2.0) clk = ~clk; end
     end
-
-
-
 endmodule : clock
