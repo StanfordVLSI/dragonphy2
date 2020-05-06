7e4f088ed49a4f492113f8c12ff046581b52d0d8
Zachary Myers <zmagnitar@gmail.com>
2020-05-06 16:08:48 -0700

    Fixed some small issues

--- tests/new_tests/MM_CDR/test_mm_cdr.py
+++ tests/new_tests/MM_CDR/test_mm_cdr.py
@@ -13,17 +13,35 @@ if 'FPGA_SERVER' in os.environ:
 else:
     SIMULATOR = 'ncsim'
 
+
+DUMP_WAVEFORMS = True
+
 @pytest.mark.wip
 def test_sim():
     deps = get_deps_cpu_sim_new(impl_file=THIS_DIR / 'test.sv')
     print(deps)
+
+    def qwrap(s):
+        return f'"{s}"'
+
+    defines = {
+        'DAVE_TIMEUNIT': '1fs',
+        'NCVLOG': None
+    }
+
+    flags = ['-unbuffered']
+    if DUMP_WAVEFORMS:
+        defines['DUMP_WAVEFORMS'] = None
+        flags += ['-access', '+r']
 
     DragonTester(
         ext_srcs=deps,
         directory=BUILD_DIR,
         top_module='test',
         inc_dirs=[get_mlingua_dir() / 'samples', get_dir('inc/new_cpu')],
-        defines={'DAVE_TIMEUNIT': '1fs', 'NCVLOG': None},
-        simulator=SIMULATOR,
-        dump_waveform=True
+        defines=defines,
+        simulator=SIMULATOR
     ).run()
+
+if __name__ == "__main__":
+    test_sim()
