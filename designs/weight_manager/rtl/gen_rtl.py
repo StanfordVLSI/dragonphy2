f58af8a7201a554ea8f45196be5315046ff9afec
Steven Herbst <sherbst@stanford.edu>
2020-05-06 15:22:58 -0700

    fix regression script and make weight_manager synthesis test more realistic

--- designs/weight_manager/rtl/gen_rtl.py
+++ designs/weight_manager/rtl/gen_rtl.py
@@ -3,12 +3,19 @@ import re
 from pathlib import Path
 from dragonphy import *
 
-TOP_CELL = os.environ.get('design_name', 'weight_manager')
 OUTPUT_FILE = 'design.v'
+
+def remove_dup(seq):
+    # fast method to remove duplicates from a list while preserving order
+    # source: Raymond Hettinger (https://twitter.com/raymondh/status/944125570534621185)
+    return list(dict.fromkeys(seq))
 
 # build up a list of source files
 src_files = []
-src_files += get_deps_new_asic(TOP_CELL)
+src_files += get_deps_new_asic(impl_file=get_file('vlog/new_chip_src/digital_core/dsp_debug_intf.sv'))
+src_files += get_deps_new_asic(impl_file=get_file('vlog/new_chip_src/weight_manager/wme_debug_intf.sv'))
+src_files += get_deps_new_asic(impl_file='weight_manager_insts.sv')
+src_files = remove_dup(src_files)
 
 # generate the output text
 output = ''
