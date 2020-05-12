4c95765020e990d1b19db9286484249fe0280604
Daniel Stanley <dstanley@Stanford.edu>
2020-05-12 13:22:01 -0700

    Test runs, still has debugging code

--- tests/new_tests/LOOPBACK_QUICK/test.sv
+++ tests/new_tests/LOOPBACK_QUICK/test.sv
@@ -1,6 +1,7 @@
 `include "mLingua_pwl.vh"
 `include "iotype.sv"
 
+// From butterphy LOOPBACK
 `default_nettype none
 
 `ifndef EXT_PI_CTL_OFFSET_0
@@ -18,6 +19,27 @@
 `ifndef EXT_PI_CTL_OFFSET_3	
 	`define EXT_PI_CTL_OFFSET_3 447
 `endif
+
+// From dragonphy AC
+`define FORCE_ADBG(name, value) force top_i.iacore.adbg_intf_i.``name`` = ``value``
+`define FORCE_DDBG(name, value) force top_i.idcore.ddbg_intf_i.``name`` = ``value``
+
+`ifndef TI_ADC_TXT
+    `define TI_ADC_TXT
+`endif
+
+`ifndef EXT_PFD_OFFSET
+    `define EXT_PFD_OFFSET 16
+`endif
+
+`ifndef N_INTERVAL
+    `define N_INTERVAL 10
+`endif
+
+`ifndef INTERVAL_LENGTH
+    `define INTERVAL_LENGTH 10e-9
+`endif
+
 
 module test;
 
@@ -52,6 +74,9 @@ module test;
 	
 	// JTAG
 	jtag_intf jtag_intf_i();
+    // TODO do we need this jtad_drv? it's instantiated down below
+    // jtag_drv jtag_drv_i (jtag_intf_i);
+
 
 	// instantiate top module
 	dragonphy_top top_i (
@@ -59,7 +84,7 @@ module test;
 		.ext_rx_inp(ch_outp),
 		.ext_rx_inn(ch_outn),
 		.ext_Vcm(v_cm),
-		.ext_Vcal(v_cal),
+		.ext_Vcal(0.23),
 
 		// clock inputs 
 		.ext_clkp(ext_clkp),
@@ -128,12 +153,52 @@ module test;
 	tx_output_recorder tx_output_recorder_i (
 		.in(tx_data),
 		.clk(tx_clk),
-		.en(1'b1)
+		//.en(1'b1)
+		.en(should_record)
 	);
 
+
+    ///////// Inserting magic reordering
+    logic signed [Nadc-1:0] adcout_unfolded [Nti-1:0];
+    reg recording_clk;
+// Re-ordering
+    // TODO: clean this up because it is likely a real bug
+    integer tmp;
+    integer idx_order [Nti] = '{0, 5, 10, 15,
+                                1, 6, 11, 12,
+                                2, 7,  8, 13,
+                                3, 4,  9, 14};
+    always @(posedge top_i.idcore.clk_adc) begin
+        // compute the unfolded ADC outputs
+        for (int k=0; k<Nti; k=k+1) begin
+            // compute output
+            tmp = top_i.idcore.adcout_sign[idx_order[k]] ?
+                  top_i.idcore.adcout[idx_order[k]] - (`EXT_PFD_OFFSET) :
+                  (`EXT_PFD_OFFSET) - top_i.idcore.adcout[idx_order[k]];
+            // clamp
+            if (tmp > 127) begin
+                tmp = 127;
+            end
+            if (tmp < -128) begin
+                tmp = -128;
+            end
+            // assign to output vector
+            adcout_unfolded[k] = tmp;
+        end
+        // pulse the recording clock
+        recording_clk = 1'b1;
+        #(1ps);
+        recording_clk = 1'b0;
+        #(1ps);
+    end
+
+    //////// End magic reordering
+
 	ti_adc_recorder ti_adc_recorder_i (
-		.in(top_i.idcore.adcout_unfolded[Nti-1:0]),
-		.clk(top_i.clk_adc),
+		//.in(top_i.idcore.adcout_unfolded[Nti-1:0]),
+		//.clk(top_i.clk_adc),
+        .in(adcout_unfolded),
+        .clk(recording_clk),
 		.en(should_record)
 	);
 
--- tests/new_tests/LOOPBACK_QUICK/test_loopback_quick.py
+++ tests/new_tests/LOOPBACK_QUICK/test_loopback_quick.py
@@ -15,8 +15,10 @@ else:
 
 #@pytest.mark.wip
 def test_sim():
+    '''
     deps = get_deps_cpu_sim_new(impl_file=THIS_DIR / 'test.sv')
     print(deps)
+
 
     DragonTester(
         ext_srcs=deps,
@@ -24,5 +26,57 @@ def test_sim():
         top_module='test',
         inc_dirs=[get_mlingua_dir() / 'samples', get_dir('inc/new_cpu')],
         defines={'DAVE_TIMEUNIT': '1fs', 'NCVLOG': None},
-        simulator=SIMULATOR
+        simulator=SIMULATOR,
+        dump_waveforms = True
     ).run()
+'''
+    # grab results
+
+    with open(BUILD_DIR / 'tx_output.txt') as f:
+        tx = f.read()
+    with open(BUILD_DIR / 'ti_adc.txt') as f:
+        adc = f.read()
+
+    tx = [int(x) for x in tx.split()]
+    adc = [int(x.replace(',', '')) for x in adc.split()]
+    adc = [a - 0.5*b for a,b in zip(adc[:-1], adc[1:])] + [0]
+
+    print(tx[:100])
+    print(adc[:100])
+
+
+    def test_shift(shift):
+        correct = 0
+        N = len(tx) - shift
+        for i in range(N):
+            correct += (tx[i] ^ (adc[i+shift]<0))
+        return correct / N
+
+    results = [test_shift(x) for x in range(2000)]
+    print(results)
+    print('BEST AND WORST', max(results), min(results))
+    shift = results.index(max(results))
+    for i in range(30):
+        pairs = zip(tx[16*i:16*(i+1)], adc[16*i+shift:16*(i+1)+shift])
+        print('\t'.join([str(t)+' '+str(a)+' ' for t,a in pairs]))
+
+    def check(t, a):
+        b = a > 0
+        if (t,b) == (0, 0):
+            return 0
+        elif (t, b) == (1, 1):
+            return 1
+        elif (t, b) == (0, 1):
+            return 2
+        elif (t,b) == (1, 0):
+            return 3
+
+
+    for i in range(30):
+        pairs = zip(tx[16*i:16*(i+1)], adc[16*i+shift:16*(i+1)+shift])
+        print('\t'.join([str(check(t, a)) for t,a in pairs]))
+
+    print('done')
+    
+if __name__ == '__main__':
+    test_sim()
