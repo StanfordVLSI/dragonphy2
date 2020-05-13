065da2d51a0a648d77c1fe77828e3c0a0d20e396
Steven Herbst <sherbst@stanford.edu>
2020-05-13 11:59:33 -0700

    syntax issues resolved

--- vlog/new_chip_src/prbs/prbs_checker.sv
+++ vlog/new_chip_src/prbs/prbs_checker.sv
@@ -1,17 +1,24 @@
+`default_nettype none
+
 module prbs_checker #(
-    parameter integer n_prbs=7,
-    parameter integer n_channels=16,
-    parameter integer n_shift_bits=$clog2(n_channels),
-    parameter integer n_match_bits=$clog2(n_channels)+1
+    parameter integer n_prbs=32,
+    parameter integer n_channels=16
 ) (
-    // clock and reset
+    // basic clock and reset for this block
     input wire logic clk,
     input wire logic rst,
 
-    // initial values for LFSRs used in PRBS
-    // these have to be picked in a certain way
-    // to ensure the right phase relationship
-    input wire logic [(n_prbs-1):0] prbs_init_vals [(n_channels-1):0],
+    // clock gating
+    input wire logic cke,
+
+    // define the PRBS equation
+    input wire logic [(n_prbs-1):0] eqn,
+
+    // bits for selecting / de-selecting certain channels from the PRBS test
+    input wire logic [(n_channels-1):0] chan_sel,
+
+    // "chicken" bits for flipping the sign of various bits
+    input wire logic [1:0] inv_chicken,
 
     // recovered data from ADC, FFE, MLSD, etc.
     input wire logic [(n_channels-1):0] rx_bits,
@@ -24,86 +31,72 @@ module prbs_checker #(
     input wire logic [1:0] checker_mode,
 
     // outputs
-    output reg [63:0] correct_bits,
-    output reg [63:0] total_bits,
-    output reg [(n_shift_bits-1):0] rx_shift
+    output wire logic [63:0] error_bits,
+    output wire logic [63:0] total_bits
 );
-
     // TODO: consider using enum here
     localparam logic [1:0]  RESET = 2'b00;
     localparam logic [1:0]  ALIGN = 2'b01;
     localparam logic [1:0]   TEST = 2'b10;
     localparam logic [1:0] FREEZE = 2'b11;
 
-    // control signals for the checker core
-    logic prbs_rst, prbs_cke, prbs_match;
-    logic [(n_match_bits-1):0] prbs_match_bits;
-
     // instantiate the core prbs checker
-    prbs_checker_core #(
-        .n_prbs(n_prbs),
-        .n_channels(n_channels),
-        .n_shift_bits(n_shift_bits)
-    ) prbs_checker_core_i (
-        .clk(clk),
-        .rst(prbs_rst),
-        .prbs_cke(prbs_cke),
-        .prbs_init_vals(prbs_init_vals),
-        .rx_bits(rx_bits),
-        .rx_shift(rx_shift),
-        .match(prbs_match),
-        .match_bits(prbs_match_bits)
-    );
+    logic [(n_channels-1):0] error_signals;
+    genvar k;
+    generate
+        for (k=0; k<n_channels; k=k+1) begin
+            prbs_checker_core #(
+                .n_prbs(n_prbs)
+            ) prbs_checker_core_i (
+                .clk(clk),
+                .rst(rst),
+                .cke(cke),
+                .eqn(eqn),
+                .inv_chicken(inv_chicken),
+                .error(error_signals[k])
+            );
+        end
+    endgenerate
 
-    // check the RX data
-    logic [1:0] err_count;
-    always @(posedge clk) begin
-        if (checker_mode == RESET) begin
-            prbs_rst <= 1;
-            prbs_cke <= 0;
-            rx_shift <= '1;
-            correct_bits <= 0;
-            total_bits <= 0;
-            err_count <= 0;
-        end else if (checker_mode == ALIGN) begin
-            prbs_rst <= 0;
-            if (prbs_match) begin
-                prbs_cke <= 1;
-                rx_shift <= rx_shift;
-                err_count <= 0;
-            end else begin
-                if (err_count == 3) begin
-                    if (rx_shift == 0) begin
-                        prbs_cke <= 0;
-                        rx_shift <= '1;
-                    end else begin
-                        prbs_cke <= 1;
-                        rx_shift <= rx_shift - 1;
-                    end
-                    err_count <= 0;
-                end else begin
-                    prbs_cke <= 1;
-                    rx_shift <= rx_shift;
-                    err_count <= err_count + 1;
-                end
-            end
-            correct_bits <= 0;
-            total_bits <= 0;
-        end else if (checker_mode == TEST) begin
-            prbs_rst <= 0;
-            prbs_cke <= 1;
-            rx_shift <= rx_shift;
-            correct_bits <= correct_bits + prbs_match_bits;
-            total_bits <= total_bits + n_channels;
-            err_count <= 0;
-        end else begin
-            prbs_rst <= prbs_rst;
-            prbs_cke <= prbs_cke;
-            rx_shift <= rx_shift;
-            correct_bits <= correct_bits;
-            total_bits <= total_bits;
-            err_count <= err_count;
+    // sum up number of errors for selected channels
+    logic [$clog2(n_channels):0] err_count;
+    logic [$clog2(n_channels):0] tot_count;
+
+    integer i;
+    always @* begin
+        err_count = 0;
+        tot_count = 0;
+        for (i=0; i<n_channels; i=i+1) begin
+            err_count = err_count + (error_signals[i] & chan_sel[i]);
+            tot_count = tot_count + chan_sel[i];
         end
     end
 
+    // check the RX data
+
+    logic [63:0] error_bits_reg;
+    logic [63:0] total_bits_reg;
+
+    always @(posedge clk) begin
+        if (checker_mode == RESET) begin
+            error_bits_reg <= 0;
+            total_bits_reg <= 0;
+        end else if (checker_mode == ALIGN) begin
+            error_bits_reg <= error_bits_reg;
+            total_bits_reg <= total_bits_reg;
+        end else if (checker_mode == TEST) begin
+            error_bits_reg <= error_bits_reg + err_count;
+            total_bits_reg <= total_bits_reg + tot_count;
+        end else begin
+            error_bits_reg <= error_bits_reg;
+            total_bits_reg <= total_bits_reg;
+        end
+    end
+
+    // assign outputs
+    assign error_bits = error_bits_reg;
+    assign total_bits = total_bits_reg;
+
 endmodule
+
+`default_nettype wire
