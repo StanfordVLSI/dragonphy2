ffced5f74fb3f5199649e41f9423ae5052a3b143
zamyers <zmagnitar@gmail.com>
2020-05-13 20:51:56 -0700

    speculative floorplan

--- designs/dragonphy_top/custom-geom/outputs/set-geom-vars.tcl
+++ designs/dragonphy_top/custom-geom/outputs/set-geom-vars.tcl
@@ -1,15 +1,28 @@
 # Variables shared between init and power steps
 
 # cell_height is from common_vars.tcl
-set cell_height 0.576
+proc snap_to_grid {input granularity} {
+   set new_value [expr ceil($input / $granularity) * $granularity]
+   return $new_value
+}
+
+set cell_height [dbGet top.fPlan.coreSite.size_y]
+set vert_pitch  [dbGet top.fPlan.coreSite.size_y]
+set horiz_pitch [dbGet top.fPlan.coreSite.size_x]
+
+set core_margin_t $vert_pitch
+set core_margin_b $vert_pitch 
+set core_margin_r [expr 5 * $horiz_pitch]
+set core_margin_l [expr 5 * $horiz_pitch]
+
 set welltap_width 0.72
 set DB_width 0.36
 
 set boundary_width [expr $welltap_width+$DB_width]
 set boundary_height [expr $cell_height]
 
-set acore_width 360.45
-set acore_height [expr 702*$cell_height]
+set acore_width [snap_to_grid 400 horiz_pitch]
+set acore_height [snap_to_grid 400 vert_pitch]
 
 set input_buffer_width 53.1
 set input_buffer_height [expr 87*$cell_height]
@@ -22,12 +35,19 @@ set memory_height 249.84
 
 set sram_width  64.175
 set sram_height 249.840
- 
+set sram_to_acore_spacing_x [snap_to_grid 40 $horiz_pitch]
+set sram_to_acore_spacing_y [snap_to_grid 40 $vert_pitch]
+
+set sram_to_sram_spacing  [snap_to_grid 15 $horiz_pitch]
+set sram_neighbor_spacing $sram_width + $sram_to_sram_spacing
+set sram_pair_spacing 2*$sram_width + $sram_to_sram_spacing
+set sram_vert_spacing [snap_to_grid 200 $vert_pitch]
+
 set blockage_width [expr 0.9]
 set blockage_height [expr 2*$cell_height]
 
-set FP_width [expr ceil(580/0.09)*0.09]
-set FP_height [expr ceil(700/$cell_height)*$cell_height]
+set FP_width [snap_to_grid 800 $horiz_pitch ]
+set FP_height [snap_to_grid 700 $vert_pitch ]
 
 set center_x [expr $FP_width/2]
 
@@ -51,5 +71,11 @@ set origin3_y [expr (ceil(260-$input_buffer_height/2)/$cell_height)*$cell_height
 set origin4_x [expr $origin3_x+$input_buffer_width/2+$blockage_width+10*$welltap_width+$DB_width]
 set origin4_y [expr ceil((87-$output_buffer_height/2)/$cell_height)*$cell_height]
 
-set origin_sram_ffe_x [expr $origin1_x+4*$blockage_width]
-set origin_sram_ffe_y [expr 2*$boundary_height+2*$blockage_height+$cell_height]
+set origin_acore_x    [expr $sram_pair_spacing + $sram_to_acore_spacing_x ]
+set origin_acore_y    [expr $sram_height + $sram_to_acore_spacing_y ]
+
+set origin_sram_ffe_x [expr $blockage_width  + $core_margin_l]
+set origin_sram_ffe_y [expr $blockage_height + $core_margin_b]
+
+set origin_sram_adc_x [expr $blockage_width  + $core_margin_l + 2*$sram_width + $sram_to_sram_spacing]
+set origin_sram_adc_y [expr $blockage_height + $core_margin_b]
--- designs/dragonphy_top/custom-init/outputs/floorplan.tcl
+++ designs/dragonphy_top/custom-init/outputs/floorplan.tcl
@@ -8,6 +8,15 @@
 # Floorplan variables
 #-------------------------------------------------------------------------
 
+proc snap_to_grid {input granularity} {
+   set new_value [expr ceil($input / $granularity) * $granularity]
+   return $new_value
+}
+
+proc round {input} {
+    set new_value [expr floor($input / )]
+}
+
 # Set the floorplan to target a reasonable placement density with a good
 # aspect ratio (height:width). An aspect ratio of 2.0 here will make a
 # rectangular chip with a height that is twice the width.
@@ -26,11 +35,12 @@ set savedvars(p_ring_width)   [expr 48 * $M1_min_width];   # Arbitrary!
 set savedvars(p_ring_spacing) [expr 24 * $M1_min_spacing]; # Arbitrary!
 
 # Core bounding box margins
+# Set in Geom-Vars File
+#set core_margin_t $vert_pitch
+#set core_margin_b $vert_pitch 
+#set core_margin_r [expr 5 * $horiz_pitch]
+#set core_margin_l [expr 5 * $horiz_pitch]
 
-set core_margin_t [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_b [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_r [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_l [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
 
 #-------------------------------------------------------------------------
 # Floorplan
@@ -41,8 +51,13 @@ set core_margin_l [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $
 # density).
 #
 
-floorPlan -r $core_aspect_ratio $core_density_target \
-             $core_margin_l $core_margin_b $core_margin_r $core_margin_t
+#floorPlan -r $core_aspect_ratio $core_density_target \
+#             $core_margin_l $core_margin_b $core_margin_r $core_margin_t
+
+
+
+floorPlan -site core -s $FP_width $FP_height \
+                        $core_margin_l $core_margin_b $core_margin_r $core_margin_t
 
 setFlipping s
 
@@ -52,21 +67,31 @@ setFlipping s
 # Place Instances #
 ###################
 
-# TODO: place multiple memory instances, i.e.
-# idcore/oneshot_multimemory_i/sram_i[k]/memory
-# idcore/omm_ffe_i/sram_i[k]/memory
-# for 0 <= k <= N_mem_tiles-1
-for {set k 0} {$k<($::env(num_mem_tiles)-1)} {incr k} {
-    placeInstance \
-        idcore/oneshot_multimemory_i/sram_i[k]/memory \
-        [expr $origin_sram_ffe_x + $k*($sram_width + 8*$welltap_width)] \
-        [expr $origin_sram_ffe_y + $sram_height    + 4*$cell_height]
+for {set k 0} {$k<4} {incr k} {
+    if {[expr $k % 2] == 0} {
+        placeInstance \
+        idcore/omm_ffe_i/genblk3_$k\__sram_i/memory \
+            [expr $origin_sram_ffe_x] \
+            [expr $origin_sram_ffe_y + ($sram_vert_spacing + $sram_height)*$k/2]
 
-    placeInstance \
-        idcore/omm_ffe_i/sram_i[k]/memory \
-        [expr $origin_sram_ffe_x + $k*($sram_width + 8*$welltap_width)] \
-        [expr $origin_sram_ffe_y ]
+        placeInstance \
+        idcore/oneshot_multimemory_i/genblk3_$k__sram_i/memory \
+            [expr $origin_sram_adc_x + $sram_pair_spacing*$k/2] \
+            [expr $origin_sram_adc_y ]
+
+    } else {
+        placeInstance \ 
+        idcore/omm_ffe_i/genblk3_$k\__sram_i/memory \
+            [expr $origin_sram_ffe_x + $sram_neighbor_spacing] \
+            [expr $origin_sram_ffe_y + ($sram_vert_spacing + $sram_height)*$k/2] MY
+
+        placeInstance \
+        idcore/oneshot_multimemory_i/genblk3_$k__sram_i/memory \
+            [expr $origin_sram_adc_x + $sram_neighbor_spacing +  $sram_pair_spacing*$k/2] \
+            [expr $origin_sram_adc_y ]
+    }
 }
+
 
 ###################
 # Place Blockages #
@@ -74,11 +99,45 @@ for {set k 0} {$k<($::env(num_mem_tiles)-1)} {incr k} {
 
 createPlaceBlockage -box \
     [expr $origin_sram_ffe_x - $blockage_width] \
-    [expr $origin_sram_ffe_y - $blockage_width] \
-    [expr $origin_sram_ffe_x + 4*$sram_width  + 24*$welltap_width + $blockage_width] \
-    [expr $origin_sram_ffe_y + 2*$sram_height + 4*$cell_height + $blockage_width]
+    [expr $origin_sram_ffe_y - $blockage_height] \
+    [expr $origin_sram_ffe_x + $sram_pair_spacing + $blockage_width] \
+    [expr $origin_sram_ffe_y + $sram_height       + $blockage_height]
 
+createPlaceBlockage -box \
+    [expr $origin_sram_ffe_x - $blockage_width] \
+    [expr $origin_sram_ffe_y + $sram_vert_spacing + $sram_height - $blockage_height] \
+    [expr $origin_sram_ffe_x + $sram_pair_spacing + $blockage_width] \
+    [expr $origin_sram_ffe_y + $sram_vert_spacing + 2*$sram_height + $blockage_height]
 
-planDesign
+createPlaceBlockage -box \
+    [expr $origin_sram_adc_x - $blockage_width] \
+    [expr $origin_sram_adc_y - $blockage_height] \
+    [expr $origin_sram_adc_x + 2*$sram_pair_spacing + $blockage_width] \
+    [expr $origin_sram_adc_y + $sram_height         + $blockage_height]
+
+createPlaceBlockage -box \
+    [expr $origin_sram_adc_x - $blockage_width] \
+    [expr $origin_sram_adc_y - $blockage_height] \
+    [expr $origin_sram_adc_x + 2*$sram_pair_spacing + $blockage_width] \
+    [expr $origin_sram_adc_y + $sram_height         + $blockage_height]
+
+createPlaceBlockage -box \
+    [expr $origin_acore_x - $blockage_width] \
+    [expr $origin_acore_y - $blockage_height] \
+    [expr $origin_acore_x + $acore_width  + $blockage_width] \
+    [expr $origin_acore_y + $acore_height + $blockage_height]
+
+#oneshot_multimemory_N_mem_tiles4_0 ffe
+#   sram_ADR_BITS10_DAT_BITS144_3 genblk3_0__sram_i 
+#   sram_ADR_BITS10_DAT_BITS144_2 genblk3_1__sram_i 
+#   sram_ADR_BITS10_DAT_BITS144_1 genblk3_2__sram_i 
+#   sram_ADR_BITS10_DAT_BITS144_0 genblk3_3__sram_i 
+
+#oneshot_multimemory_N_mem_tiles4_1 adc
+#   sram_ADR_BITS10_DAT_BITS144_7 genblk3_0__sram_i
+#   sram_ADR_BITS10_DAT_BITS144_6 genblk3_1__sram_i
+#   sram_ADR_BITS10_DAT_BITS144_5 genblk3_2__sram_i
+#   sram_ADR_BITS10_DAT_BITS144_4 genblk3_3__sram_i
+
 
 
