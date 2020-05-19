7b1a9f14bc82469852697c78cdfe81afeb020f03
Zachary Myers <zmagnitar@gmail.com>
2020-05-19 03:45:44 -0700

    Fixed Buffer Size Issues

--- vlog/new_chip_src/digital_core/dsp_backend.sv
+++ vlog/new_chip_src/digital_core/dsp_backend.sv
@@ -25,11 +25,11 @@ module dsp_backend (
 	localparam integer ffe_pipeline_depth         = 3;
 	localparam integer ffe_code_pipeline_depth    = ffe_code_numPastBuffers + ffe_code_numFutureBuffers + 1;
 	localparam integer cmp_pipeline_depth         = mlsd_bit_numPastBuffers + mlsd_bit_numFutureBuffers + 1;
-	localparam integer code_pipeline_depth        = ffe_code_pipeline_depth + ffe_pipeline_depth + cmp_pipeline_depth;
+	localparam integer code_pipeline_depth        = ffe_code_pipeline_depth + ffe_pipeline_depth + cmp_pipeline_depth + 4;
 	localparam integer mlsd_code_pipeline_depth   = mlsd_code_numPastBuffers + mlsd_code_numFutureBuffers + 1;
 
 	localparam integer ffe_code_start             = 0;
-	localparam integer mlsd_code_start 			  = ffe_pipeline_depth + ffe_pipeline_depth + (cmp_pipeline_depth-mlsd_code_pipeline_depth);
+	localparam integer mlsd_code_start 			  = ffe_pipeline_depth + ffe_code_pipeline_depth + (cmp_pipeline_depth-mlsd_code_pipeline_depth);
 
     localparam integer pb_buffer_depth            = 5;
 	//Connecting Wires
@@ -186,6 +186,11 @@ module dsp_backend (
 	);
 
 	logic signed [mlsd_gpack::code_precision-1:0] est_seq [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
+    logic signed [mlsd_gpack::code_precision-1:0] est_seq_0 [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
+    logic signed [mlsd_gpack::code_precision-1:0] est_seq_1 [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
+    logic signed [mlsd_gpack::code_precision-1:0] est_seq_2 [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
+    logic signed [mlsd_gpack::code_precision-1:0] est_seq_3 [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
+
 	logic signed [mlsd_gpack::code_precision-1:0] precalc_seq_vals [2**mlsd_gpack::bit_length-1:0][constant_gpack::channel_width-1:0][mlsd_gpack::length-1:0];
 
 	seq_val_gen #(
@@ -213,6 +218,16 @@ module dsp_backend (
 
 		.est_seq_out(est_seq)
 	);
+
+
+   always @(posedge clk) begin
+        est_seq_0 <= est_seq;
+        est_seq_1 <= est_seq_0 ;
+        est_seq_2 <= est_seq_1 ;
+        est_seq_3 <= est_seq_2 ;
+    end
+
+
 
 	wire logic   	  [mlsd_gpack::code_precision-1:0] flat_ucodes_mlsd [mlsd_gpack::width*mlsd_code_pipeline_depth-1:0];
 	flatten_buffer_slice #(
@@ -245,7 +260,7 @@ module dsp_backend (
 		.cbit(mlsd_gpack::est_center)
 	) comb_mlsd_dec_i (
 		.flat_codes  (flat_codes_mlsd),
-		.est_seq     (est_seq),
+		.est_seq     (est_seq_3),
 		.shift_index (mlsd_shift),
 		.predict_bits(predict_bits)
 	);
