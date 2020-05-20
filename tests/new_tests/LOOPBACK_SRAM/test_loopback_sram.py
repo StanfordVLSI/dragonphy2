c84cbc75facbbf00c1c1aec68ff5a0dd8e0d0fdd
Daniel Stanley <dstanley@Stanford.edu>
2020-05-19 19:16:30 -0700

    Edit python to handle don't care values for unused sram reads

--- tests/new_tests/LOOPBACK_SRAM/test_loopback_sram.py
+++ tests/new_tests/LOOPBACK_SRAM/test_loopback_sram.py
@@ -17,7 +17,7 @@ else:
 
 # Set DUMP_WAVEFORMS to True if you want to dump all waveforms for this
 # test.  The waveforms are stored in tests/new_tests/AC/build/waves.shm
-DUMP_WAVEFORMS = True
+DUMP_WAVEFORMS = False
 
 @pytest.mark.parametrize((), [pytest.param(marks=pytest.mark.slow) if SIMULATOR=='vivado' else ()])
 def test_sim():
@@ -40,6 +40,8 @@ def test_sim():
         defines['DUMP_WAVEFORMS'] = None
         flags += ['-access', '+r']
 
+    # NOTE This times out after 10us, after reading 1312*16
+    # bits back from SRAM.
     DragonTester(
         ext_srcs=deps,
         directory=BUILD_DIR,
@@ -54,13 +56,21 @@ def test_sim():
     tx = np.loadtxt(BUILD_DIR / 'tx_output.txt', dtype=int, delimiter=',')
     tx = tx.flatten()
 
-    adc = np.loadtxt(BUILD_DIR / 'sram_out.txt', dtype=int, delimiter=',')
+    # reading file is a little strange because sram captures
+    # 8 sets of data per cycle and we only care about first 16
+    # The last 2 are "x" so we can't use default np loader
+    NUM_ADC = 16
+    adc_txt = open(BUILD_DIR / 'sram_out.txt').readlines()
+    adc2 = [line.strip().split(',') for line in adc_txt]
+    adc3 = [line[:NUM_ADC] for line in adc2]
+    #adc = np.loadtxt(BUILD_DIR / 'sram_out.txt', dtype=int, delimiter=',')
+    adc = np.array(adc3, dtype=int)
     adc = adc.flatten()
 
     # what fraction of bits are correct when latency is 'shift'?
     def test_shift(shift):
         correct = 0
-        N = len(tx) - shift
+        N = min(len(tx), len(adc)) - shift
         for i in range(N):
             correct += (tx[i] ^ (adc[i+shift]<0))
         return correct / N
