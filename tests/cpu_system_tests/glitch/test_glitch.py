6f9d2572761f0240648969770e7826bdf810d620
Steven Herbst <sherbst@stanford.edu>
2020-05-29 17:29:42 -0700

    fix glitch test

--- tests/cpu_system_tests/glitch/test_glitch.py
+++ tests/cpu_system_tests/glitch/test_glitch.py
@@ -8,7 +8,7 @@ THIS_DIR = Path(__file__).parent.resolve()
 BUILD_DIR = THIS_DIR / 'build'
 SIMULATOR = 'ncsim'
 
-def test_sim():
+def test_sim(dump_waveforms):
     deps = get_deps_cpu_sim(impl_file=THIS_DIR / 'test.sv')
     print(deps)
 
