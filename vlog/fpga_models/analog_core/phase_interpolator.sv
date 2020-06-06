a610169ea080a4e3ed2a7bb977bd3390d6a70fdf
Steven Herbst <sherbst@stanford.edu>
2020-06-05 22:55:16 -0700

    all of the previous synthesizable analog models have been adapted for dragonphy and put back in place

--- vlog/fpga_models/analog_core/phase_interpolator.sv
+++ vlog/fpga_models/analog_core/phase_interpolator.sv
@@ -1,4 +1,6 @@
-module phase_interpolator #( 
+`include "svreal.sv"
+
+module phase_interpolator #(
     parameter Nbit = 9,
     parameter Nctl_dcdl = 2,
     parameter Nunit = 32,
@@ -35,16 +37,46 @@ module phase_interpolator #(
     output cal_out_dmm,
     output [19:0]  pm_out
 );
+    // signals use for external I/O
+    (* dont_touch = "true" *) logic signed [((`DT_WIDTH)-1):0] dt_req;
+    (* dont_touch = "true" *) logic signed [((`DT_WIDTH)-1):0] emu_dt;
+    (* dont_touch = "true" *) logic emu_clk;
+    (* dont_touch = "true" *) logic emu_rst;
 
+    // format for emu_dt / dt_req
+    `REAL_FROM_WIDTH_EXP(DT_FMT, `DT_WIDTH, `DT_EXPONENT);
+
+    // set maximum value for timestep
+    `REAL_FROM_WIDTH_EXP(dt_req_max, `DT_WIDTH, `DT_EXPONENT);
+    `ASSIGN_CONST_REAL(((2.0**((`DT_WIDTH)-1))-1.0)*2.0**(`DT_EXPONENT), dt_req_max);
+
+    // instantiate MSDSL model, passing through format information
+    clk_delay_core #(
+        `PASS_REAL(emu_dt, DT_FMT),
+        `PASS_REAL(dt_req, DT_FMT),
+        `PASS_REAL(dt_req_max, dt_req_max)
+    ) clk_delay_core_i (
+        // main I/O: delay code, clock in/out values
+        .code(ctl),
+        .clk_i_val(clk_in),
+        .clk_o_val(clk_out_slice),
+        // timestep control: DT request and response
+        .dt_req(dt_req),
+        .emu_dt(emu_dt),
+        // emulator clock and reset
+        .emu_clk(emu_clk),
+        .emu_rst(emu_rst),
+        // additional input: maximum timestep
+        .dt_req_max(dt_req_max)
+    );
+
+    // outputs that are not modeled
     assign cal_out = 0;
-    assign clk_out_slice = 0;
     assign clk_out_sw = 0;
     assign del_out = 0;
-
     assign Qperi = 0;
     assign max_sel_mux = 0;
     assign cal_out_dmm = 0;
     assign pm_out = 0;
-
 endmodule
 
