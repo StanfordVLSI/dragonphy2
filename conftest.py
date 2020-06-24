e2679d96d09208a313beaa078bedbede3ba4021c
Steven Herbst <sherbst@stanford.edu>
2020-06-24 13:42:22 -0700

    add emu_clk_freq and ffe_length options

--- conftest.py
+++ conftest.py
@@ -13,6 +13,14 @@ def pytest_addoption(parser):
         '--ser_port', default='/dev/ttyUSB2', type=str, help='USB serial path.'
     )
 
+    parser.addoption(
+        '--ffe_length', default=4, type=int, help='Number of FFE coefficients per channel.'
+    )
+
+    parser.addoption(
+        '--emu_clk_freq', default=5.0e6, type=float, help='Frequency of emulator clock (Hz)'
+    )
+
 @pytest.fixture
 def dump_waveforms(request):
     return request.config.getoption('--dump_waveforms')
@@ -24,3 +32,11 @@ def board_name(request):
 @pytest.fixture
 def ser_port(request):
     return request.config.getoption('--ser_port')
+
+@pytest.fixture
+def ffe_length(request):
+    return request.config.getoption('--ffe_length')
+
+@pytest.fixture
+def emu_clk_freq(request):
+    return request.config.getoption('--emu_clk_freq')
