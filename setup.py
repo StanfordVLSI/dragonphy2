8a135fa014673f100a94af1b220b8741aa42329e
Steven Herbst <sherbst@stanford.edu>
2020-07-28 17:36:40 -0700

    add instructions for generating TAP EDIF [skip ci]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires_list = [
     # anasymod ecosystem
     'svreal==0.2.4',
     'msdsl==0.3.1',
-    'anasymod==0.3.2',
+    'anasymod==0.3.3',
     # system-verilog parser
     'svinst==0.1.5',
     # magma ecosystem dependencies
