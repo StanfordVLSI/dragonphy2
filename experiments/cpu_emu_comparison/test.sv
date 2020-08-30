80bf6645f6e2c6939d83e65be3f249451abe338a
Steven Herbst <sherbst@stanford.edu>
2020-08-30 12:28:22 -0700

    add more experimental options

--- experiments/cpu_emu_comparison/test.sv
+++ experiments/cpu_emu_comparison/test.sv
@@ -102,6 +102,28 @@ module test;
         .out(tx_data)
     );
 
+// uncomment to try using the same PRBS stimulus used in the emulator
+
+//    logic tx_rst;
+//    initial begin
+//        tx_rst = 1'b1;
+//        #(1ns);
+//        tx_rst = 1'b0;
+//    end
+//
+//    prbs_generator_syn #(
+//        .n_prbs(32)
+//    ) prbs_generator_syn_i (
+//        .clk(tx_clk),
+//        .rst(tx_rst),
+//        .cke(1'b1),
+//        .init_val(32'h00000001),
+//        .eqn(32'h100002),
+//        .inj_err(1'b0),
+//        .inv_chicken(2'b00),
+//        .out(tx_data)
+//    );
+
 // Uncomment to use a pattern generator that is
 // useful for debugging the sequence of operations
 
