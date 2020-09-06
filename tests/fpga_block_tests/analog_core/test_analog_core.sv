9e5a3640f1394165fd1dff8647763a397b200dbd
Steven Herbst <sherbst@stanford.edu>
2020-09-05 17:42:48 -0700

    try re-running tests

--- tests/fpga_block_tests/analog_core/test_analog_core.sv
+++ tests/fpga_block_tests/analog_core/test_analog_core.sv
@@ -55,24 +55,25 @@ module test_analog_core import const_pack::*; #(
     assign ctl_pi[2] = ctl_pi_2;
     assign ctl_pi[3] = ctl_pi_3;
 
-    // wire adder_out
+    // wire adder_out (first output is not used but is X, which is
+    // not handled by fault)
     logic [Nadc-1:0] adder_out [(Nti-1):0];
-    assign adder_out_0 = adder_out[0];
-    assign adder_out_1 = adder_out[1];
-    assign adder_out_2 = adder_out[2];
-    assign adder_out_3 = adder_out[3];
-    assign adder_out_4 = adder_out[4];
-    assign adder_out_5 = adder_out[5];
-    assign adder_out_6 = adder_out[6];
-    assign adder_out_7 = adder_out[7];
-    assign adder_out_8 = adder_out[8];
-    assign adder_out_9 = adder_out[9];
-    assign adder_out_10 = adder_out[10];
-    assign adder_out_11 = adder_out[11];
-    assign adder_out_12 = adder_out[12];
-    assign adder_out_13 = adder_out[13];
-    assign adder_out_14 = adder_out[14];
-    assign adder_out_15 = adder_out[15];
+    assign adder_out_0  = (^adder_out[ 0]===1'bx) ? 0 : adder_out[ 0];
+    assign adder_out_1  = (^adder_out[ 1]===1'bx) ? 0 : adder_out[ 1];
+    assign adder_out_2  = (^adder_out[ 2]===1'bx) ? 0 : adder_out[ 2];
+    assign adder_out_3  = (^adder_out[ 3]===1'bx) ? 0 : adder_out[ 3];
+    assign adder_out_4  = (^adder_out[ 4]===1'bx) ? 0 : adder_out[ 4];
+    assign adder_out_5  = (^adder_out[ 5]===1'bx) ? 0 : adder_out[ 5];
+    assign adder_out_6  = (^adder_out[ 6]===1'bx) ? 0 : adder_out[ 6];
+    assign adder_out_7  = (^adder_out[ 7]===1'bx) ? 0 : adder_out[ 7];
+    assign adder_out_8  = (^adder_out[ 8]===1'bx) ? 0 : adder_out[ 8];
+    assign adder_out_9  = (^adder_out[ 9]===1'bx) ? 0 : adder_out[ 9];
+    assign adder_out_10 = (^adder_out[10]===1'bx) ? 0 : adder_out[10];
+    assign adder_out_11 = (^adder_out[11]===1'bx) ? 0 : adder_out[11];
+    assign adder_out_12 = (^adder_out[12]===1'bx) ? 0 : adder_out[12];
+    assign adder_out_13 = (^adder_out[13]===1'bx) ? 0 : adder_out[13];
+    assign adder_out_14 = (^adder_out[14]===1'bx) ? 0 : adder_out[14];
+    assign adder_out_15 = (^adder_out[15]===1'bx) ? 0 : adder_out[15];
 
     // instantiate the debug interface (although it is unused)
     acore_debug_intf adbg_intf_i ();
