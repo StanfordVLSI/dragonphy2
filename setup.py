b2855155ad9446bf9390553dfbab9604472fb5e4
Steven Herbst <sherbst@stanford.edu>
2020-09-18 12:40:29 -0700

    bump anasymod version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires_list = [
     # anasymod ecosystem
     'svreal==0.2.6',
     'msdsl==0.3.4',
-    'anasymod==0.3.5.dev7',
+    'anasymod==0.3.5',
     # system-verilog parser
     'svinst==0.1.5',
     # magma ecosystem dependencies
