24332705d14697d4b6c6d1e9e5e39b14f1f31a34
zamyers <zmagnitar@gmail.com>
2020-09-29 15:10:12 -0700

    Added a test function to test_sparams that plots all the sparams. One of the sparam files is not compatible

--- tests/other_tests/sparams/test_sparams.py
+++ tests/other_tests/sparams/test_sparams.py
@@ -24,3 +24,24 @@ def test_sparams():
     plt.savefig(THIS_DIR / 'step.eps')
     plt.cla()
     plt.clf()
+
+def test_all_impulses(): 
+    THIS_DIR = Path(__file__).resolve().parent
+   
+    sparam_file_list = ["peters_01_0605_B12_thru.s4p",
+                        "peters_01_0605_B1_thru.s4p",  
+                        "peters_01_0605_T20_thru.s4p",
+                        "TEC_Whisper42p8in_Meg6_THRU_C8C9.s4p",
+                        "TEC_Whisper42p8in_Nelco6_THRU_C8C9.s4p"]
+
+
+    for sparam_file in sparam_file_list:
+        file_name = str(get_file(f'data/channel_sparam/{sparam_file}'))
+        t, imp = s4p_to_impulse(file_name, 0.1e-12, 20e-9, zs=50, zl=50)
+        plt.plot(t*1e9, imp, label=sparam_file.split('.')[0])
+
+    plt.legend()
+    plt.savefig(THIS_DIR / 'all_imp.eps')
+    plt.cla()
+    plt.clf()
+
