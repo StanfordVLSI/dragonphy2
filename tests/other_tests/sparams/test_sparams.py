a4815156597af7c541c8eef446894f51a5e36a0f
Steven Herbst <sherbst@stanford.edu>
2020-09-29 17:11:15 -0700

    add example showing pulse response

--- tests/other_tests/sparams/test_sparams.py
+++ tests/other_tests/sparams/test_sparams.py
@@ -3,44 +3,13 @@ import matplotlib.pyplot as plt
 
 from dragonphy import *
 
-def test_sparams():
-    THIS_DIR = Path(__file__).resolve().parent
-    file_name = str(get_file('data/channel_sparam/peters_01_0605_B12_thru.s4p'))
+THIS_DIR = Path(__file__).resolve().parent
+S4P_DIR = get_dir('data/channel_sparam')
 
-    t, imp = s4p_to_impulse(file_name, 0.1e-12, 20e-9, zs=50, zl=50)
-    plt.plot(t*1e9, imp)
-    plt.title('Impulse Response')
-    plt.xlabel('Time (ns)')
-    plt.ylabel('Value')
-    plt.savefig(THIS_DIR / 'imp.eps')
-    plt.cla()
-    plt.clf()
-
-    t, step = s4p_to_step(file_name, 0.1e-12, 20e-9, zs=50, zl=50)
-    plt.plot(t*1e9, step)
-    plt.title('Step Response')
-    plt.xlabel('Time (ns)')
-    plt.ylabel('Value')
-    plt.savefig(THIS_DIR / 'step.eps')
-    plt.cla()
-    plt.clf()
-
-def test_all_impulses(): 
-    THIS_DIR = Path(__file__).resolve().parent
-   
-    sparam_file_list = [
-        "Case4_FM_13SI_20_T_D13_L6.s4p",
-        "TEC_Whisper42p8in_Meg6_THRU_C8C9.s4p",
-        "TEC_Whisper42p8in_Nelco6_THRU_C8C9.s4p",
-        "peters_01_0605_B12_thru.s4p",
-        "peters_01_0605_B1_thru.s4p",
-        "peters_01_0605_T20_thru.s4p",
-    ]
-
-    for sparam_file in sparam_file_list:
-        file_name = str(get_file(f'data/channel_sparam/{sparam_file}'))
-        t, imp = s4p_to_impulse(file_name, 0.1e-12, 20e-9, zs=50, zl=50)
-        plt.plot(t*1e9, imp, label=sparam_file.split('.')[0])
+def test_all_impulses():
+    for file_name in S4P_DIR.glob('*.s4p'):
+        t, imp = s4p_to_impulse(str(file_name), 0.1e-12, 20e-9, zs=50, zl=50)
+        plt.plot(t*1e9, imp, label=file_name.stem)
 
     plt.legend(prop={'size': 8})
     plt.title('All Impulse Responses')
@@ -50,3 +19,30 @@ def test_all_impulses():
     plt.cla()
     plt.clf()
 
+def test_all_steps():
+    for file_name in S4P_DIR.glob('*.s4p'):
+        t, imp = s4p_to_step(str(file_name), 0.1e-12, 20e-9, zs=50, zl=50)
+        plt.plot(t*1e9, imp, label=file_name.stem)
+
+    plt.legend(prop={'size': 8})
+    plt.title('All Step Responses')
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Value')
+    plt.savefig(THIS_DIR / 'all_step.eps')
+    plt.cla()
+    plt.clf()
+
+def test_all_pulses():
+    for file_name in S4P_DIR.glob('*.s4p'):
+        chan = Channel(channel_type='s4p', sampl_rate=10e12, resp_depth=200000,
+                       s4p=file_name, zs=50, zl=50)
+        _, pulse = chan.get_pulse_resp(f_sig=16e9, resp_depth=10000, t_delay=0)
+        plt.plot(pulse, '-o', label=file_name.stem, markersize=2)
+
+    plt.legend(prop={'size': 8})
+    plt.title('All Pulse Responses')
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.savefig(THIS_DIR / 'all_pulse.eps')
+    plt.cla()
+    plt.clf()
