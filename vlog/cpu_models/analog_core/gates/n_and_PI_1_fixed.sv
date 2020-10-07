a793c7ee524d2df4f5e03740170da66537e923c8
Sung-Jin Kim <sjkim85@stanford.edu>
2020-10-07 14:53:44 -0700

    output buffer connection and cpu model of n_and_PI_1_fixed are updated

---
+++ vlog/cpu_models/analog_core/gates/n_and_PI_1_fixed.sv
@@ -0,0 +1,56 @@
+/********************************************************************
+filename: inv_PI_1_fixed.sv
+
+Description: 
+a parameterized inv cell 
+
+Assumptions:
+
+Todo:
+
+********************************************************************/
+
+module n_and_PI_1_fixed #(
+    parameter real td_nom = 7.50e-12,    // nominal delay in sec
+    parameter real td_std = 0.0,         // std dev of nominal delay variation in sec
+    parameter real rj_rms = 0.0          // rms random jitter in sec
+) (
+    input wire logic in1,                 // input signal
+    input wire logic in2,                 // input signal
+    output wire out                      // delayed output signal
+);
+
+timeunit 1fs;
+timeprecision 1fs;
+
+import model_pack::Delay;
+
+// design parameter class
+
+Delay dly_obj;
+
+// variables
+
+real td;    // delay w/o jitter
+real rj;    // random jitter
+
+// initialize class parameters
+initial begin
+    dly_obj = new(td_nom, td_std);
+    td = dly_obj.td;
+end
+
+///////////////////////////
+// Model Body
+///////////////////////////
+
+// compute new jitter value
+always @(in1 or in2) begin
+    rj = dly_obj.get_rj(rj_rms);
+end
+
+// assign to the output using an inertial delay
+// ref: http://www-inst.eecs.berkeley.edu/~cs152/fa06/handouts/CummingsHDLCON1999_BehavioralDelays_Rev1_1.pdf
+assign #((td+rj)*1s) out = ~(in1 & in2);
+
+endmodule
