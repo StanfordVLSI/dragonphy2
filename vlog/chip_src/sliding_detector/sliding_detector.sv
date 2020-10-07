2a0fe45cfe2d8298f1f086dd67dc24bb1d8f2e2b
Zachary Myers <zmagnitar@gmail.com>
2020-10-07 15:17:39 -0700

    Move files into their new homes :) + fix syntax problems with sliding detector

--- vlog/chip_src/sliding_detector/sliding_detector.sv
+++ vlog/chip_src/sliding_detector/sliding_detector.sv
@@ -3,34 +3,29 @@ module sliding_detector #(
     parameter integer width=16,
     parameter integer est_error_bitwidth=8,
     parameter integer est_channel_bitwidth=8,
+    parameter integer max_bitwidth=8,
     parameter integer sliding_detector_depth=2
 ) (
-    input logic signed [est_errror_bitwidth-1:0]  errstream [width*sliding_detector_depth-1:0],
+    input logic signed [est_error_bitwidth-1:0]  errstream [width*sliding_detector_depth-1:0],
     input logic                                   bitstream [width*sliding_detector_depth-1:0],
     input logic signed [est_channel_bitwidth-1:0] channel [seq_length-1:0],
 
-    output logic signed [est_bitwidth*2+2-1:0] sqr_inj_error [3:0][width-1:0][seq_length-1:0]
+    output logic signed [est_error_bitwidth*2+2-1:0] sqr_inj_error [3:0][width-1:0][seq_length-1:0],
     output logic [1:0] mmse_err_pos [width-1:0]
 );
 
     logic signed [est_channel_bitwidth-1:0] error [width:0][seq_length:0];
-    generate
-        if(est_error_bitwidth > est_channel_bitwidth) begin
-            logic signed [est_error_bitwidth*2+4-1:0] sqr_error [1:0][width-1:0][seq_length:0];
-            logic  [est_error_bitwidth*2+4+$clog2(seq_length)-1:0] mse_err [3:0][width-1:0];
-        end else begin
-            logic signed [est_channel_bitwidth*2+4-1:0] sqr_error [1:0][width-1:0][seq_length:0];
-            logic  [est_channel_bitwidth*2+4+$clog2(seq_length)-1:0] mse_err [3:0][width-1:0];
-        end
-    endgenerate
+    logic signed [max_bitwidth*2+4-1:0] sqr_error [1:0][width-1:0][seq_length:0];
+    logic        [max_bitwidth*2+4+$clog2(seq_length)-1:0] mse_err [3:0][width-1:0];
 
-    logic  [1:0] int_mmse_err_pos [1:0][width-1:0];
+    logic        [max_bitwidth*2+4+$clog2(seq_length)-1:0] mmse_err [width-1:0];
+
 
     integer ii,jj,kk;
     always_comb begin
         //Select the correct polarity of the injected inverse-error-vector
         for(ii=0; ii<width+1; ii=ii+1) begin
-            for(jj=0; jj<seq_length+1; jj=jj=1) begin
+            for(jj=0; jj<seq_length+1; jj=jj+1) begin
                 error[ii][jj] = bitstream[ii] ? channel[jj] : -channel[jj];
             end
         end
@@ -54,16 +49,18 @@ module sliding_detector #(
         for(kk=0; kk<4; kk=kk+1) begin
             for(ii=0; ii<width; ii=ii+1) begin
                 for(jj=0; jj<seq_length; jj=jj+1) begin
-                    mse_err[kk][ii] = mse_err[kk][ii] + sqr_inj_err[kk][ii][jj];
+                    mse_err[kk][ii] = mse_err[kk][ii] + sqr_inj_error[kk][ii][jj];
                 end
             end
         end
         //Rank the sum square errors and return the position of the smallest error
         for(ii=0; ii<width; ii=ii+1) begin
-            int_mmse_err_pos[0][ii] = mse_err[0][ii] < mse_err[1][ii] ? 0 : 1;
-            int_mmse_err_pos[1][ii] = mse_err[2][ii] < mse_err[3][ii] ? 2 : 3;
-            mmse_err_pos[ii]        = mse_err[int_mmse_err_pos[0]][ii] < mse_err[int_mmse_err_pos[1]][ii] ? 
-                                      int_mmse_err_pos[0][ii] : int_mmse_err_pos[1][ii];
+            mmse_err[ii] = mse_err[0][ii];
+            mmse_err_pos[ii] = 0;
+            for(jj=1; jj<4; jj=jj+1) begin
+                mmse_err[ii] = (mmse_err[ii] > mse_err[jj][ii]) ? mse_err[jj][ii] : mmse_err[ii];
+                mmse_err_pos[ii] = (mmse_err[ii] > mse_err[jj][ii]) ? jj : mmse_err_pos[ii];
+            end 
         end
     end
 
