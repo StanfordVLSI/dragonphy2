187727f75e3032194e012251b674fce7674c2a6e
zamyers <zmagnitar@gmail.com>
2020-10-08 14:11:27 -0700

    lol have to merge the other side :C

--- vlog/chip_src_v2/test.sv
+++ vlog/chip_src_v2/test.sv
@@ -1,4 +1,5 @@
 module test ();
+    dsp_debug_intf dsp_dbg_intf_i();
 
     datapath_core #(
         .ffe_pipeline_depth(1), 
@@ -6,6 +7,5 @@ module test ();
         .additional_error_pipeline_depth(0), 
         .sliding_detector_output_pipeline_depth(1)) dp_core_i (.dsp_dbg_intf_i(dsp_dbg_intf_i));
 
-    dsp_debug_intf dsp_dbg_intf_i();
 
 endmodule : test
