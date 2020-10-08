82458e195f3d1478dd802153f8645b2545144357
zamyers <zmagnitar@gmail.com>
2020-10-08 13:59:27 -0700

    second typo

--- vlog/chip_src/datapath_core/datapath_core.sv
+++ vlog/chip_src/datapath_core/datapath_core.sv
@@ -36,7 +36,7 @@ module datapath_core #(
     logic signed [ffe_gpack::weight_precision-1:0] weights [constant_gpack::channel_width-1:0][ffe_gpack::length-1:0];
     logic [ffe_gpack::shift_precision-1:0] ffe_shift [constant_gpack::channel_width-1:0];
     logic signed [cmp_gpack::thresh_precision-1:0] thresh  [constant_gpack::channel_width-1:0];
-    logic signed [channel_gpack::est_estimate_precision-1:0] channel_est [constant_gpack::channel_width-1:0][channel_gpack::est_channel_depth-1:0];
+    logic signed [channel_gpack::est_channel_precision-1:0] channel_est [constant_gpack::channel_width-1:0][channel_gpack::est_channel_depth-1:0];
     logic [channel_gpack::shift_precision-1:0] channel_shift [constant_gpack::channel_width-1:0];
     logic [constant_gpack::channel_width-1:0] disable_product [ffe_gpack::length-1:0];
 
