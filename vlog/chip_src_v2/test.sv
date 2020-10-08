a3d520b112fa1a9a6491cc7e667f7f1768eeb836
Zachary Myers <zmagnitar@gmail.com>
2020-10-08 14:10:24 -0700

    Committing to merge

--- vlog/chip_src_v2/test.sv
+++ vlog/chip_src_v2/test.sv
@@ -4,7 +4,7 @@ module test ();
         .ffe_pipeline_depth(1), 
         .channel_pipeline_depth(1), 
         .additional_error_pipeline_depth(0), 
-        .sliding_detector_output_pipeline_depth(1)) dp_core_i (.dsp_dbg_intf_i());
+        .sliding_detector_output_pipeline_depth(1)) dp_core_i (.dsp_dbg_intf_i(dsp_dbg_intf_i));
 
     dsp_debug_intf dsp_dbg_intf_i();
 
