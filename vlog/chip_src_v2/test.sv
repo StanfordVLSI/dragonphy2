01ce61e89fdee0d37a990609fb444b6f1f765774
Zachary Myers <zmagnitar@gmail.com>
2020-10-08 14:10:48 -0700

    Trying to fix erro

--- vlog/chip_src_v2/test.sv
+++ vlog/chip_src_v2/test.sv
@@ -1,11 +1,11 @@
 module test ();
 
+    dsp_debug_intf dsp_dbg_intf_i();
     datapath_core #(
         .ffe_pipeline_depth(1), 
         .channel_pipeline_depth(1), 
         .additional_error_pipeline_depth(0), 
         .sliding_detector_output_pipeline_depth(1)) dp_core_i (.dsp_dbg_intf_i(dsp_dbg_intf_i));
 
-    dsp_debug_intf dsp_dbg_intf_i();
 
 endmodule : test
