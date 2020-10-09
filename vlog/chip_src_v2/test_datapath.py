7a8676f5338687bc84acb78356321a4b592010b6
Zachary Myers <zmagnitar@gmail.com>
2020-10-09 09:32:37 -0700

    Reflect changes backwards

--- vlog/chip_src_v2/test_datapath.py
+++ vlog/chip_src_v2/test_datapath.py
@@ -4,7 +4,7 @@ from dragonphy import *
 THIS_DIR=Path(__file__).parent.resolve()
 BUILD_DIR = THIS_DIR /'build'
 
-deps = get_deps_asic(impl_file=THIS_DIR/'test.sv')
+deps = get_deps_cpu_sim(impl_file=THIS_DIR/'test.sv')
 DragonTester(
     ext_srcs=deps,
     directory=BUILD_DIR
