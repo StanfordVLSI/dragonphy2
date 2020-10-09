a7443d4ede60c60d59f0fe3e3481c8d594393e0a
Zachary Myers <zmagnitar@gmail.com>
2020-10-09 10:46:09 -0700

    Tried to use fault, produces incomprehensible error :C

--- vlog/chip_src_v2/test_fault_datapath.py
+++ vlog/chip_src_v2/test_fault_datapath.py
@@ -8,22 +8,22 @@ from dragonphy import *
 THIS_DIR = Path(__file__).parent.resolve()
 BUILD_DIR = THIS_DIR / 'build'
 
-class dut(m.circuit):
+class dut(m.Circuit):
     name = 'test'
     io = m.IO(
         rstb=m.BitIn,
         clk=m.ClockIn,
-        ffe_weights=m.In(m.Array[10, m.Bits[8]]),
+        ffe_weights=m.In(m.Array[10, m.Bits[10]]),
         channel_est=m.In(m.Array[30, m.Bits[8]]),
-        adc_codes  =m.In(m.Array[16, m.bits[8]])
+        adc_codes  =m.In(m.Array[16, m.Bits[8]])
     )
 
 t = fault.Tester(dut, dut.clk)
 t.zero_inputs()
-t.poke(rstb, 0)
+t.poke(dut.rstb, 0)
 
 for ii in range(16):
-    t.poke(adc_codes[ii], ii)
+    t.poke(dut.adc_codes[ii], ii)
 
 t.compile_and_run(
     target='system-verilog',
