3e5afcf0c424e40e215e6a5b731743bc565b57ee
CansWang <canswang@stanford.edu>
2020-10-10 19:51:18 -0700

    Remove duplicated blocks and testbench

--- vlog/chip_src/tx_16t1/mux.sv
+++
@@ -1,8 +0,0 @@
-`timescale 100ps/1ps  // Remove this line before synthesis
-
-module mux (input in0, input in1, input sel, output out);
-// #0.2; // Remove this line before synthesis, Tcp delay ~ 2FO4
- assign  out = sel ? in1:in0;
-endmodule
-
-
