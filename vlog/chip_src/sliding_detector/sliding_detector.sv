268ab032abdd96588f23ce5af41fb8877e80f497
zamyers <zmagnitar@gmail.com>
2020-10-11 18:00:50 -0700

    Fixing Sliding detector's pipeline

--- vlog/chip_src/sliding_detector/sliding_detector.sv
+++ vlog/chip_src/sliding_detector/sliding_detector.sv
@@ -5,7 +5,8 @@ module sliding_detector #(
     parameter integer est_error_bitwidth=8,
     parameter integer est_channel_bitwidth=8,
     parameter integer max_bitwidth=8,
-    parameter integer sliding_detector_depth=2
+    parameter integer sliding_detector_depth=2,
+    parameter integer t0_buff=1
 ) (
     input logic signed [est_error_bitwidth-1:0]  errstream [width*sliding_detector_depth-1:0],
     input logic                                   bitstream [width*sliding_detector_depth-1:0],
@@ -14,6 +15,8 @@ module sliding_detector #(
     output logic signed [est_error_bitwidth*2+2-1:0] sqr_inj_error [3:0][width-1:0][seq_length-1:0],
     output logic [1:0] mmse_err_pos [width-1:0]
 );
+
+    localparam idx = t0_buff * width;
 
     logic signed [est_channel_bitwidth-1:0] error [width:0][seq_length:0];
     logic signed [max_bitwidth*2+4-1:0] sqr_error [1:0][width-1:0][seq_length:0];
@@ -25,13 +28,13 @@ module sliding_detector #(
     integer ii,jj,kk;
     always_comb begin
         //Select the correct polarity of the injected inverse-error-vector
-        for(ii=0; ii<width; ii=ii+1) begin
+        for(ii=1; ii<width+1; ii=ii+1) begin
             for(jj=0; jj<seq_length+1; jj=jj+1) begin
-                error[ii][jj] = bitstream[ii] ? channel[ii][jj] : -channel[ii][jj];
+                error[ii][jj] = bitstream[idx+ii-1] ? channel[ii-1][jj] : -channel[ii-1][jj];
             end
         end
         for(jj=0; jj<seq_length+1; jj=jj+1) begin
-            error[width][jj] = bitstream[width] ? channel[0][jj] : -channel[0][jj];
+            error[0][jj] = bitstream[idx-1] ? channel[width-1][jj] : -channel[width-1][jj];
         end
 
         //Inject an IEV at the relevant position and then square the result
