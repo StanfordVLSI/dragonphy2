098e417eae202db17e6d8a31fd8faa7d19ddb379
Zach Myers <zamyers@DN51skkd.SUNet>
2020-10-19 12:38:24 -0700

    fixing pipeline back to zero

--- vlog/chip_src_v2/test.sv
+++ vlog/chip_src_v2/test.sv
@@ -79,10 +79,10 @@ module test ();
 
     dsp_debug_intf dsp_dbg_intf_i();
     datapath_core #(
-        .ffe_pipeline_depth(5), //constant_gpack::ffe_pipeline_depth), 
-        .channel_pipeline_depth(10), 
-        .error_output_pipeline_depth(1), 
-        .sliding_detector_output_pipeline_depth(5)
+        .ffe_pipeline_depth(constant_gpack::ffe_pipeline_depth), 
+        .channel_pipeline_depth(constant_gpack::channel_pipeline_depth), 
+        .error_output_pipeline_depth(constant_gpack::error_output_pipeline_depth), 
+        .sliding_detector_output_pipeline_depth(constant_gpack::sliding_detector_output_pipeline_depth)
     ) dp_core_i (
         .adc_codes(adc_codes),
         .clk(clk),
