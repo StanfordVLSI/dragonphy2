fbaade268f95bbb1138518a0629c0e139e933d60
Zach Myers <zamyers@DN51skkd.SUNet>
2020-10-19 14:20:57 -0700

    Creating debug interface for error tracker

---
+++ vlog/chip_src/error_tracker/error_tracker_debug_intf.sv
@@ -0,0 +1,30 @@
+`default_nettype none
+
+interface error_tracker_debug_intf;
+
+	import const_pack::*;
+
+	logic [addrwidth-1:0] addr;
+	logic read;
+	logic enable;
+
+	logic [31:0] output_data_frame[4:0];
+
+	modport tracker (
+		input addr,
+		input read,
+		input enable,
+		output output_data_frame
+	);
+
+	modport  jtag (
+	 output addr,
+	 output read,
+	 output enable,
+	 input output_data_frame
+
+	);
+
+endinterface
+
+`default_nettype wire
