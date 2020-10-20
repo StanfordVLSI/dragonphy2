e2f13497a34a65f19a547177eb60e4b2c252c817
Zach Myers <zamyers@DN51skkd.SUNet>
2020-10-19 22:36:56 -0700

    Restructuring error tracker to simplify top design

--- vlog/chip_src_v2/test.sv
+++ vlog/chip_src_v2/test.sv
@@ -71,7 +71,7 @@ module test ();
     logic signed [channel_gpack::est_channel_precision-1:0] channel_est [channel_gpack::est_channel_depth-1:0];
     logic signed [ffe_gpack::weight_precision-1:0] ffe_weights [ffe_gpack::length-1:0];
     logic bits [constant_gpack::channel_width-1:0];
-    logic trigger;
+    logic [15:0] prbs_flags;
     logic clk, rstb, start;
 
 
@@ -97,91 +97,6 @@ module test ();
         .dsp_dbg_intf_i(dsp_dbg_intf_i)
     );
 
-
-    logic sliced_bits_buffer [constant_gpack::channel_width-1:0][2:0];
-    logic signed [error_gpack::est_error_precision-1:0] est_error_buffer [constant_gpack::channel_width-1:0][2:0];
-    logic [1:0] sd_flags_buffer [constant_gpack::channel_width-1:0][2:0];
-    
-    //Bits Pipeline
-    buffer #(
-        .numChannels (constant_gpack::channel_width),
-        .bitwidth    (1),
-        .depth       (2)
-    ) sb_buff_i (
-        .in      (sliced_bits),
-        .clk     (clk),
-        .rstb    (1'b1),
-        .buffer  (sliced_bits_buffer)
-    );
-
-    signed_buffer #(
-        .numChannels (constant_gpack::channel_width),
-        .bitwidth    (error_gpack::est_error_precision),
-        .depth       (2)
-    ) ee_buff_i (
-        .in      (est_error),
-        .clk     (clk),
-        .rstb    (1'b1),
-        .buffer  (est_error_buffer)
-    );
-
-    buffer #(
-        .numChannels (constant_gpack::channel_width),
-        .bitwidth    (2),
-        .depth       (2)
-    ) sf_buff_i (
-        .in      (sd_flags),
-        .clk     (clk),
-        .rstb    (1'b1),
-        .buffer  (sd_flags_buffer)
-    );
-
-    logic flat_sliced_bits [constant_gpack::channel_width*3-1:0];
-    logic signed [error_gpack::est_error_precision-1:0] flat_est_error   [constant_gpack::channel_width*3-1:0];
-    logic [1:0] flat_sd_flags [constant_gpack::channel_width*3-1:0];
-
-    logic [constant_gpack::channel_width*3-1:0] pf_sliced_bits;
-    
-    flatten_buffer_slice #(
-        .numChannels(constant_gpack::channel_width),
-        .bitwidth   (1),
-        .buff_depth (2),
-        .slice_depth(2),
-        .start      (0)
-    ) sb_fb_i (
-        .buffer    (sliced_bits_buffer),
-        .flat_slice(flat_sliced_bits)
-    );
-
-    signed_flatten_buffer_slice #(
-        .numChannels(constant_gpack::channel_width),
-        .bitwidth   (error_gpack::est_error_precision),
-        .buff_depth (2),
-        .slice_depth(2),
-        .start      (0)
-    ) ee_fb_i (
-        .buffer    (est_error_buffer),
-        .flat_slice(flat_est_error)
-    );
-
-    flatten_buffer_slice #(
-        .numChannels(constant_gpack::channel_width),
-        .bitwidth   (2),
-        .buff_depth (2),
-        .slice_depth(2),
-        .start      (0)
-    ) sf_fb_i (
-        .buffer    (sd_flags_buffer),
-        .flat_slice(flat_sd_flags)
-    );
-
-    genvar gi, gj;
-    generate 
-        for(gi =0 ; gi < constant_gpack::channel_width*3; gi = gi + 1) begin
-            assign pf_sliced_bits[gi] = flat_sliced_bits[gi]; 
-        end
-    endgenerate
-
     error_tracker_debug_intf #(.addrwidth(12)) errt_dbg_intf_i();
 
     error_tracker #(
@@ -189,11 +104,9 @@ module test ();
         .error_bitwidth(error_gpack::est_error_precision),
         .addrwidth(12)
     ) errt_i (
-        .trigger(trigger),
-
-        .prbs_flags(),
-        .errors(flat_est_error),
-        .bitstream(pf_sliced_bits),
+        .prbs_flags(prbs_flags),
+        .est_error(est_error),
+        .sliced_bits(sliced_bits),
         .sd_flags(flat_sd_flags),
 
         .clk(clk),
@@ -281,7 +194,7 @@ module test ();
     initial begin
         rstb = 0;
         start = 0;
-        trigger = 0;
+        prbs_flags = 4'h0000;
         errt_dbg_intf_i.addr = 0;
         errt_dbg_intf_i.read = 0;
         errt_dbg_intf_i.enable = 0;
@@ -297,13 +210,13 @@ module test ();
         repeat (5) @(posedge clk);
         rstb = 1;
         repeat (3) @(posedge clk);
-        trigger = 1;
+        prbs_flags = 4'h0101;
         repeat (2) @(posedge clk);
-        trigger = 0;
+        prbs_flags = 4'h0000;
         repeat (10) @(posedge clk);
-        trigger = 1;
+        prbs_flags = 4'h1010;
         repeat (3) @(posedge clk); 
-        trigger =0 ;
+        prbs_flags = 4'h0000;
         repeat (10) @(posedge clk);
         errt_dbg_intf_i.read = 1;
         @(posedge clk);
