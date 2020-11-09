b023073bb8eb9e3ed2635b37a04808f8dc287a3b
Can WANG <canswang@stanford.edu>
2020-11-08 16:53:30 -0800

    Another attempt

--- dragonphy/views.py
+++ dragonphy/views.py
@@ -159,6 +159,7 @@ def get_deps_asic(cell_name=None, impl_file=None, process='tsmc16'):
         override['tx_tri_buf'] = 'chip_src_freepdk45'
         override['mux_2'] = 'chip_src_freepdk45'
         override['tx_inv'] = 'chip_src_freepdk45'
+        override['qr_mux_fixed'] = 'chip_src_freepdk45'
         skip.add('INV_X4')
         skip.add('TBUF_X4')
         skip.add('sram_144_1024_freepdk45')
