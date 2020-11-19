314073723cd128bd94be05b4ff99133ba9bb9a38
Steven Herbst <sherbst@stanford.edu>
2020-11-18 20:19:44 -0800

    updates to example SDF simulation

--- experiments/sdf_simulation/opt.f
+++ experiments/sdf_simulation/opt.f
@@ -1,3 +1,6 @@
 ../../build/mflowgen_dragonphy_top/4-freepdk-45nm/view-standard/stdcells.v
 design.vcs.v
 test.sv
+-top test
+-sdf_cmd_file sdf.cmd
+-timescale 1ns/1fs
--- experiments/sdf_simulation/test.sv
+++ experiments/sdf_simulation/test.sv
@@ -1,12 +1,83 @@
+`timescale 1ns/1fs
+
 module test;
+
+    localparam real pi_freq=4.0e9;
 
 	dragonphy_top top_i (
 	);
 
+    initial begin
+        force top_i.itx.clk_interp_slice[0] = 1'b0;
+        #((0.00/pi_freq)*1s);
+        forever begin
+            force top_i.itx.clk_interp_slice[0] = 1'b1;
+            #((0.5/pi_freq)*1s);
+            force top_i.itx.clk_interp_slice[0] = 1'b0;
+            #((0.5/pi_freq)*1s);
+        end
+    end
+
+    initial begin
+        force top_i.itx.clk_interp_slice[1] = 1'b0;
+        #((0.25/pi_freq)*1s);
+        forever begin
+            force top_i.itx.clk_interp_slice[1] = 1'b1;
+            #((0.5/pi_freq)*1s);
+            force top_i.itx.clk_interp_slice[1] = 1'b0;
+            #((0.5/pi_freq)*1s);
+        end
+    end
+
+    initial begin
+        force top_i.itx.clk_interp_slice[2] = 1'b0;
+        #((0.50/pi_freq)*1s);
+        forever begin
+            force top_i.itx.clk_interp_slice[2] = 1'b1;
+            #((0.5/pi_freq)*1s);
+            force top_i.itx.clk_interp_slice[2] = 1'b0;
+            #((0.5/pi_freq)*1s);
+        end
+    end
+
+    initial begin
+        force top_i.itx.clk_interp_slice[3] = 1'b0;
+        #((0.75/pi_freq)*1s);
+        forever begin
+            force top_i.itx.clk_interp_slice[3] = 1'b1;
+            #((0.5/pi_freq)*1s);
+            force top_i.itx.clk_interp_slice[3] = 1'b0;
+            #((0.5/pi_freq)*1s);
+        end
+    end
+
 	initial begin
         // Set up probing
-        // $shm_open("waves.shm");
-        // $shm_probe(top_i.idcore.tx_data_gen_i);
+        // TODO: add your signals of interest here!
+        $shm_open("waves.shm");
+        $shm_probe(top_i.itx.din);
+        $shm_probe(top_i.itx.rst_BAR);
+        $shm_probe(top_i.itx.clk_interp_slice[0]);
+        $shm_probe(top_i.itx.clk_interp_slice[1]);
+        $shm_probe(top_i.itx.clk_interp_slice[2]);
+        $shm_probe(top_i.itx.clk_interp_slice[3]);
+        $shm_probe(top_i.itx.clk_halfrate);
+        $shm_probe(top_i.itx.clk_prbsgen);
+        $shm_probe(top_i.itx.mtb_n);
+        $shm_probe(top_i.itx.mtb_p);
+        $shm_probe(top_i.itx.qr_data_n);
+        $shm_probe(top_i.itx.qr_data_p);
+
+        // Initialize
+        force top_i.itx.din = 16'd42; // TODO: your data here!
+        force top_i.itx.rst_BAR = 1'b0;
+
+        // De-assert reset
+        #(((10.0)/pi_freq)*1s);
+        force top_i.itx.rst_BAR = 1'b1;
+        #(((10.0)/pi_freq)*1s);
+
+        // TODO: run test longer / try other things?
 
 		// Finish test
 		$display("Test complete.");
