6d721446773c460f3bcbea8a7a5ba8c214397794
Zachary Myers <zmagnitar@gmail.com>
2024-01-15 20:08:57 -0800

    Wrote TB for trigger based compresssed data tracking

--- vlog/chip_src/trig_tracker/trig_tracker.sv
+++ vlog/chip_src/trig_tracker/trig_tracker.sv
@@ -1,6 +1,6 @@
 module trig_tracker #(
     parameter integer bitwidth = 8,
-    parameter integer counter_bitwidth = 10
+    parameter integer counter_bitwidth = 10,
     parameter integer memory_addr_width = 10
 ) (
     input logic clk,
@@ -38,18 +38,6 @@ module trig_tracker #(
         end
     end
 
-
-    // Generate trigger pulse
-    logic trigger_prev, internal_trigger;
-    always_ff @(posedge clk or negedge rstb) begin
-        if (rstb == 1'b0) begin
-            trigger_prev <= 1'b0;
-        end else begin
-            trigger_prev <= trigger;
-        end
-    end
-    assign internal_trigger = trigger ^ trigger_prev;
-
     // Counter that resets on trigger
     logic [counter_bitwidth-1:0] counter, counter_next;
     logic counter_overflow;
@@ -58,7 +46,7 @@ module trig_tracker #(
     assign counter_next = (counter_overflow == 1'b1) ? 0 : counter + 1;
 
     always_ff @(posedge clk or negedge rstb) begin
-        if (rstb) begin
+        if (rstb == 1'b0) begin
             counter <= 0;
         end else begin
             if (data_event[1]) begin
@@ -81,61 +69,60 @@ module trig_tracker #(
     logic [counter_bitwidth+2-1:0] mem_data_init;
     logic [counter_bitwidth+2-1:0] mem_data_in_run;
 
-    always_comb begin
-        mem_data_in_run = {data_event, counter};
-        mem_data_init[counter_bitwidth+1:counter_bitwidth] = 2'b01;
-        mem_data_init[counter_bitwidth-1:bitwidth] = 0;
-        mem_data_init[bitwidth-1:0] = in;
-
-        if (ctrl_state == WRITE_ON_EVENT) begin
-            mem_data_in = mem_data_in_run;
-        end else begin
-            mem_data_in = mem_data_init;
-        end
-    end
-
 	// SRAM address muxing
 
 	logic WEB;
-	logic [memory_addr_width-1:0] write_addr, [memory_addr_width-1:0] addr;
+	logic [memory_addr_width-1:0] write_addr, addr;
 
 	assign addr = (WEB == 1'b0) ? write_addr : in_addr;
 
 	typedef enum logic [1:0] {WAIT_FOR_START, WAIT_FOR_EVENT, WRITE_ON_EVENT, MEM_FULL} ctrl_state_t;
 	ctrl_state_t ctrl_state;
 
+    always_comb begin
+        mem_data_in_run = {data_event, counter};
+        mem_data_init[counter_bitwidth+1:counter_bitwidth] = 2'b01;
+        mem_data_init[counter_bitwidth-1:bitwidth] = 0;
+        mem_data_init[bitwidth-1:0] = in;
+    end
+
 	always @(posedge clk) begin
 		if (rstb == 1'b0) begin
 			WEB <= 1'b1;
 			write_addr <= 'd0;
-			ctrl_state <= WAIT_FOR_WRITE;
+			ctrl_state <= WAIT_FOR_START;
 		end else begin
 			case (ctrl_state)
 				WAIT_FOR_START : begin
 					WEB <= (trigger == 1'b1) ? 1'b0 : 1'b1;
 					write_addr <= 'd0;
 					ctrl_state <= (trigger == 1'b1) ? WAIT_FOR_EVENT : WAIT_FOR_START;
+                    mem_data_in <= mem_data_init;
 				end
                 WAIT_FOR_EVENT : begin
                     WEB <= (mem_write_trigger == 1'b1) ? 1'b0 : 1'b1;
                     write_addr <= (mem_write_trigger == 1'b1) ? write_addr + 1 : write_addr;
                     ctrl_state <= (mem_write_trigger == 1'b1) ? WRITE_ON_EVENT : WAIT_FOR_EVENT;
+                    mem_data_in <= mem_data_in_run;
                 end
 				WRITE_ON_EVENT : begin
-					WEB <= 1'b1;
-					write_addr <= write_addr;
-					ctrl_state <= (write_addr == max_addr) ? MEM_FULL : WAIT_FOR_EVENT;
+					WEB <= (mem_write_trigger == 1'b1) ? 1'b0 : 1'b1;
+					write_addr <= (mem_write_trigger == 1'b1) ? write_addr + 1 : write_addr;
+					ctrl_state <= (write_addr == max_addr) ? MEM_FULL : (mem_write_trigger == 1'b1) ? WRITE_ON_EVENT : WAIT_FOR_EVENT;
+                    mem_data_in <= mem_data_in_run;
 				end
 				MEM_FULL : begin
 					WEB <= 1'b1;
 					write_addr <= max_addr;
 					ctrl_state <= (trigger == 1'b0) ? WAIT_FOR_START : MEM_FULL;
+                    mem_data_in <= mem_data_in;
 				end
 				default : begin
 					// same as WAIT_FOR_WRITE
 					WEB <= (trigger == 1'b1) ? 1'b0 : 1'b1;
 					write_addr <= 'd0;
-					ctrl_state <= (trigger == 1'b1) ? WRITE_ON_EVENT : WAIT_FOR_START;
+					ctrl_state <= (trigger == 1'b1) ? WAIT_FOR_EVENT : WAIT_FOR_START;
+                    mem_data_in <= mem_data_init;
 				end	 
 			endcase
 		end
